a7899fff4205c9bcdc31f362a36d89b353a79c46
Faisal Alvi <faisalalvi33@gmail.com>
2019-09-09 15:16:34 +0530

    debugging exhibitors

--- themes/nabshow-lv/functions.php
+++ themes/nabshow-lv/functions.php
@@ -91,7 +91,7 @@ require get_template_directory() . '/inc/common-functions.php';
  */
 require get_template_directory() . '/inc/ads-view-list-data.php';
 
-add_filter( 'mime_types', 'wpse_mime_types' );
+add_filter( 'upload_mimes', 'wpse_mime_types' );
 function wpse_mime_types( $existing_mimes ) {
 	// Add csv to the list of allowed mime types
 	$existing_mimes['csv'] = 'text/csv';
