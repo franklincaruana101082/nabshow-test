fc0139f47782613c9e129437feb57b3254226a5b
Nitish Kaila <kailanitish90@gmail.com>
2019-11-12 13:22:05 +0530

    added lodash dependency in .babelrc file

--- themes/nabshow-lv/inc/action/actions-functions.php
+++ themes/nabshow-lv/inc/action/actions-functions.php
@@ -16,7 +16,7 @@ function nabshow_lv_add_block_editor_assets() {
 
 	wp_register_script( 'nab-gutenberg-block',
 		get_template_directory_uri() . '/blocks/js/block.build.js',
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components', 'wp-dom-ready', 'lodash' )
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components', 'wp-dom-ready' )
 	);
 
 	wp_register_style(
