dd1d34eaaac6e1aaa424e6563317a269b086fc29
Nitish Kaila <kailanitish90@gmail.com>
2019-11-20 18:30:33 +0530

    Removed version in block js

--- themes/nabshow-lv/inc/action/actions-functions.php
+++ themes/nabshow-lv/inc/action/actions-functions.php
@@ -16,12 +16,14 @@ function nabshow_lv_add_block_editor_assets() {
 
 	wp_register_script( 'nab-gutenberg-block',
 		get_template_directory_uri() . '/blocks/js/block.build.js',
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components', 'wp-dom-ready' )
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components', 'wp-dom-ready' ),
+        null
 	);
 
 	wp_enqueue_script( 'nab-custom-gutenberg-block',
 		get_template_directory_uri() . '/blocks/js/nabshow-block.build.js',
-		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components' )
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components' ),
+        null
 	);
 
 	wp_register_style(
