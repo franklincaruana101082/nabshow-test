1b969f742e06f385e80d223521119d86474f8fdc
Nitish Kaila <kailanitish90@gmail.com>
2020-03-25 12:20:15 +0530

    Added new marketo form id

--- themes/nabshow-lv/inc/action/theme-setup-functions.php
+++ themes/nabshow-lv/inc/action/theme-setup-functions.php
@@ -285,7 +285,7 @@ function nabshow_lv_scripts() {
 
 	//Marketo script
 	wp_enqueue_script( 'nabshow-lv-marketo', '//app-ab34.marketo.com/js/forms2/js/forms2.min.js', array( 'nabshow-lv-custom' ), null, true );
-	wp_add_inline_script( 'nabshow-lv-marketo', 'MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1033);MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1091);MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1099);');
+	wp_add_inline_script( 'nabshow-lv-marketo', 'MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1033);MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1091);MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1099);MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1111);');
 
 	wp_enqueue_script( 'nabshow-lv-webreg', 'https://app.webreg.me/communities/0a61a16a0610/engagements.js', array( 'nabshow-lv-custom' ), null, true );
 }
