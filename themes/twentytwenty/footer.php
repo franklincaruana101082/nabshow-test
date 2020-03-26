5ced27484560761dd30c9ed5abe68937310abe93
Valerie DuBreuil <vdubreuil@nab.org>
2020-03-26 10:57:42 -0400

    Update footer

--- themes/twentytwenty/footer.php
+++ themes/twentytwenty/footer.php
@@ -25,14 +25,9 @@
 								_x( 'Y', 'copyright date format', 'twentytwenty' )
 							);
 							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+							<a href="https://nab.org/" target="_blank">National Association of Broadcasters</a>. All Rights Reserved.
 						</p><!-- .footer-copyright -->
-
-						<p class="powered-by-wordpress">
-							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
-							</a>
-						</p><!-- .powered-by-wordpress -->
+						
 
 					</div><!-- .footer-credits -->
 
