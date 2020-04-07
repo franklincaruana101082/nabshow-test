7a3c72bd58a8179a10af2e3344365c8c4c574718
Valerie DuBreuil <vdubreuil@nab.org>
2020-04-07 15:39:38 -0400

    footer tweaks

--- themes/twentytwenty/footer.php
+++ themes/twentytwenty/footer.php
@@ -18,7 +18,9 @@
 
 					<div class="footer-credits">
 					
-					<p class="footer-related">Related Shows: <a style="color: #ffffff;" href="https://nabshow.com" target="_blank" rel="noopener noreferrer">NAB Show</a> | <a style="color: #ffffff;" href="https://nabshowny.com" target="_blank" rel="noopener noreferrer">NAB Show New York</a> | <a style="color: #ffffff;" href="https://cineemerge.nabshow.com/" target="_blank" rel="noopener noreferrer">CineEmerge</a></p>
+					<div class="footer-related">
+					<p>Related Shows: <a style="color: #ffffff;" href="https://nabshow.com" target="_blank" rel="noopener noreferrer">NAB Show</a> | <a style="color: #ffffff;" href="https://nabshowny.com" target="_blank" rel="noopener noreferrer">NAB Show New York</a> | <a style="color: #ffffff;" href="https://cineemerge.nabshow.com/" target="_blank" rel="noopener noreferrer">CineEmerge</a></p>
+					</div>
 
 						<p class="footer-copyright">&copy;
 							<?php
