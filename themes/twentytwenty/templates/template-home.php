5ef241c2d03e4882086c57303f35a0320e7475ac
Valerie DuBreuil <vdubreuil@nab.org>
2020-05-11 17:26:30 -0400

    home template

---
+++ themes/twentytwenty/templates/template-home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Homepage Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
+ */
+
+<iframe src="http://static.frequency.com/nab-player/index.html?id=6646834083015971138" width="800" height=450" frameborder="no"></iframe>
+
+get_template_part( 'singular' );
