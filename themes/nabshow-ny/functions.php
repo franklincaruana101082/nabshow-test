938335f0159652735b1dd7e1fb35c4b129d16130
Clinton Hall <clinton@marketingdesigngroup.com>
2020-05-18 14:03:03 -0400

    cta css styling adjustments. css enqueue filemtime stamp added (if this causes issues we can just revert).

--- themes/nabshow-ny/functions.php
+++ themes/nabshow-ny/functions.php
@@ -1,5 +1,13 @@
 <?php
+
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
 function enqueue_parent_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '1.1' );
+
+	// wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '1.1' );
+
+	$css_mod_time = filemtime(get_stylesheet_directory() . '/style.css');
+
+	wp_enqueue_style ( 'parent-style', get_stylesheet_directory_uri() . '/style.css', false, $css_mod_time );
+
 }
