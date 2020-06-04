4ce8d54720e7f1d76d01cccbbb1fccdd6fed125e
Valerie DuBreuil <vdubreuil@nab.org>
2020-06-04 09:57:52 -0400

    Updating header of Exhibitor Blog and revising footer for MP

---
+++ themes/Avada-Child-Theme/header.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Header template.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+?>
+<!DOCTYPE html>
+<html class="<?php avada_the_html_class(); ?>" <?php language_attributes(); ?>>
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MQKPWN');</script>
+<!-- End Google Tag Manager -->
+
+	<?php Avada()->head->the_viewport(); ?>
+
+	<?php wp_head(); ?>
+
+	<?php
+	/**
+	 * The setting below is not sanitized.
+	 * In order to be able to take advantage of this,
+	 * a user would have to gain access to the database
+	 * in which case this is the least of your worries.
+	 */
+	echo apply_filters( 'avada_space_head', Avada()->settings->get( 'space_head' ) ); // phpcs:ignore WordPress.Security.EscapeOutput
+	?>
+</head>
+
+<?php
+$object_id      = get_queried_object_id();
+$c_page_id      = Avada()->fusion_library->get_page_id();
+$wrapper_class  = 'fusion-wrapper';
+$wrapper_class .= ( is_page_template( 'blank.php' ) ) ? ' wrapper_blank' : '';
+?>
+<body <?php body_class(); ?> <?php fusion_element_attributes( 'body' ); ?>>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQKPWN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+	<?php do_action( 'avada_before_body_content' ); ?>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'Avada' ); ?></a>
+
+	<div id="boxed-wrapper">
+		<div class="fusion-sides-frame"></div>
+		<div id="wrapper" class="<?php echo esc_attr( $wrapper_class ); ?>">
+			<div id="home" style="position:relative;top:-1px;"></div>
+			<?php avada_header_template( 'below', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
+			<?php if ( 'left' === fusion_get_option( 'header_position' ) || 'right' === fusion_get_option( 'header_position' ) ) : ?>
+				<?php avada_side_header(); ?>
+			<?php endif; ?>
+
+			<?php avada_sliders_container(); ?>
+
+			<?php avada_header_template( 'above', ( is_archive() || Avada_Helper::bbp_is_topic_tag() ) && ! ( class_exists( 'WooCommerce' ) && is_shop() ) ); ?>
+
+			<div class="avada-page-titlebar-wrapper">
+				<?php avada_current_page_title_bar( $c_page_id ); ?>
+			</div>
+
+			<?php
+			$row_css    = '';
+			$main_class = '';
+
+			if ( apply_filters( 'fusion_is_hundred_percent_template', false, $c_page_id ) ) {
+				$row_css    = 'max-width:100%;';
+				$main_class = 'width-100';
+			}
+
+			if ( fusion_get_option( 'content_bg_full' ) && 'no' !== fusion_get_option( 'content_bg_full' ) ) {
+				$main_class .= ' full-bg';
+			}
+			do_action( 'avada_before_main_container' );
+			?>
+			<main id="main" class="clearfix <?php echo esc_attr( $main_class ); ?>">
+				<div class="fusion-row" style="<?php echo esc_attr( $row_css ); ?>">
--- themes/blask-wpcom/footer.php
+++ themes/blask-wpcom/footer.php
@@ -10,15 +10,12 @@
 ?>
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="site-info">
-				Related Shows: <a href="https://nabshow.com" target="_blank">NAB Show</a> | <a href="https://nabshowny.com" target="_blank">NAB Show New York</a> | <a href="https://cineemerge.com" target="_blank">CineEmerge</a>
+Related Shows: <a href="https://nabshow.com" target="_blank">NAB Show</a> | <a href="https://nabshowny.com" target="_blank">NAB Show New York</a> | <a href="https://cineemerge.com" target="_blank">CineEmerge</a>
 <br /><br />
 
 &copy; <?php echo date("Y"); ?> National Association of Broadcasters. All Rights Reserved.
-<a href="/partners/privacy-policy/">Privacy Policy</a> | <a href="/partners/terms-of-use/">Terms of Use</a>
-<br /><br />
-
-<i>This website uses cookies to enhance your user experience. Read our privacy policy and terms of use for more information.</i>
+<br />
+<i>This website uses cookies to enhance your user experience. Read our <a href="/partners/privacy-policy/">privacy policy</a> and <a href="/partners/terms-of-use/">terms of use</a> for more information.</i>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 
