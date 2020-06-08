639fa7634e47a611d66712b26eca2680b6fc418b
Clinton Hall <clinton@marketingdesigngroup.com>
2020-06-08 11:33:26 -0400

    [chore] enqueue scripts and various styling adjustments.

--- themes/nabshow-ny/assets/js/main.js
+++ themes/nabshow-ny/assets/js/main.js
@@ -1,5 +1,78 @@
-var $ = require('jquery');
+// var $ = require('jquery');
 
 $(function() {
-	// alert('It works!');
+
+	// alert('It works! The Child!');
+	$('.faderIn').css('opacity', 0);
+		//set a waypoint for all the page parts on the page
+		var ppWaypoint = $('.faderIn').waypoint(function(direction) {
+			//check the direction
+			if(direction == 'down') {
+				//add the class to start the animation
+				$(this.element).addClass('animated fadeIn');
+				// $('.fader-sub').addClass('animated fadeInUp delay-1s');
+				//then destroy this waypoint, we don't need it anymore
+				this.destroy();
+			}
+		}, {
+			//Set the offset
+			offset: '70%'
+	});
+
+
+	$('.faderUp').css('opacity', 0);
+		//set a waypoint for all the page parts on the page
+		var ppWaypoint = $('.faderUp').waypoint(function(direction) {
+			//check the direction
+			if(direction == 'down') {
+				//add the class to start the animation
+				$(this.element).addClass('animated fadeInUp');
+				// $('.fader-sub').addClass('animated fadeInUp delay-1s');
+				//then destroy this waypoint, we don't need it anymore
+				this.destroy();
+			}
+		}, {
+			//Set the offset
+			offset: '70%'
+	});
+
+	$('.faderLeft').css('opacity', 0);
+		//set a waypoint for all the page parts on the page
+		var ppWaypoint = $('.faderLeft').waypoint(function(direction) {
+			//check the direction
+			if(direction == 'down') {
+				//add the class to start the animation
+				$(this.element).addClass('animated fadeInLeft');
+				// $('.fader-sub').addClass('animated fadeInUp delay-1s');
+				//then destroy this waypoint, we don't need it anymore
+				this.destroy();
+			}
+		}, {
+			//Set the offset
+			offset: '70%'
+	});
+
+	$('.faderRight').css('opacity', 0);
+		//set a waypoint for all the page parts on the page
+		var ppWaypoint = $('.faderRight').waypoint(function(direction) {
+			//check the direction
+			if(direction == 'down') {
+				//add the class to start the animation
+				$(this.element).addClass('animated fadeInRight');
+				// $('.fader-sub').addClass('animated fadeInUp delay-1s');
+				//then destroy this waypoint, we don't need it anymore
+				this.destroy();
+			}
+		}, {
+			//Set the offset
+			offset: '70%'
+	});
+
+
 });
+
+
+/////
+
+
+
--- themes/nabshow-ny/functions.php
+++ themes/nabshow-ny/functions.php
@@ -1,13 +1,39 @@
 <?php
 
+// PARENT STYLE
+
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 
 function enqueue_parent_styles() {
 
-	// wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '1.1' );
-
-	$css_mod_time = filemtime(get_stylesheet_directory() . '/style.css');
-
-	wp_enqueue_style ( 'parent-style', get_stylesheet_directory_uri() . '/style.css', false, $css_mod_time );
+	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '1.1' );
 
 }
+
+// WAYPOINTS
+
+add_action('wp_enqueue_scripts', 'waypoint_enqueue_script');
+
+function waypoint_enqueue_script() {
+
+  wp_enqueue_script('jquery-3', '//code.jquery.com/jquery-3.4.1.min.js', array(), '1.0.0', true);
+  
+  wp_enqueue_script('way-points', '//cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js', array(), '1.0.0', true);
+
+}
+
+// THEME PAGE
+
+add_action( 'wp_enqueue_scripts', 'ny_theme_scripts' );
+
+function ny_theme_scripts() {
+
+    $css_mod_time = filemtime(get_stylesheet_directory() . '/style.css');
+
+	wp_enqueue_style ( 'style', get_stylesheet_directory_uri() . '/style.css', false, $css_mod_time );
+
+    wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array(), '2.0.0', true );
+
+}
+
+?>
