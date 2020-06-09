3a4b6a5c911ec5c9978c763241dfae1a8a64ef7b
Valerie DuBreuil <vdubreuil@nab.org>
2020-06-09 16:45:23 -0400

    sending to html index

--- themes/cine-html/index.php
+++ themes/cine-html/index.php
@@ -1,3 +1,8 @@
+
+<html>
 <?php /* Template Name: CineEmerge */ ?>
 
-            < ?php header("Location: https://nabshow.com/cine2019/index.html"); ?>
+<?php
+header('Location: index.html');
+exit;
+?>
