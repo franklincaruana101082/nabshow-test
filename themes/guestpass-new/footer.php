326e3714bfbdfa900ab68b488f2929580621a1c2
Faisal Alvi <faisalalvi33@gmail.com>
2020-06-29 18:10:07 +0530

    adding new theme

---
+++ themes/guestpass-new/footer.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the opening of the #site-footer div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0.0
+ */
+
+?>
+			<footer id="site-footer" role="contentinfo" class="header-footer-group">
+
+				<div class="section-inner">
+
+					<div class="footer-credits">
+					
+					
+						
+					
+						<p class="footer-copyright">
+							<span class="footer-related">Related Shows: <a style="color: #ffffff;" href="https://nabshow.com" target="_blank" rel="noopener noreferrer">NAB Show</a> | <a style="color: #ffffff;" href="https://nabshowny.com" target="_blank" rel="noopener noreferrer">NAB Show New York</a> | <a style="color: #ffffff;" href="https://cineemerge.nabshow.com/" target="_blank" rel="noopener noreferrer">CineEmerge</a><br /><br />
+							</span>
+
+						&copy;
+							<?php
+							echo date_i18n(
+								/* translators: Copyright date format, see https://secure.php.net/date */
+								_x( 'Y', 'copyright date format', 'twentytwenty' )
+							);
+							?>
+							<a href="https://nab.org/" target="_blank">National Association of Broadcasters</a>. All Rights Reserved.<br />
+							<span class="footer-related">
+							<a href="/express/privacy-policy/">Privacy Policy</a> | <a href="/express/terms-of-use/">Terms of Use</a> |  <a href="/express/partners/exhibitor-resources/">Exhibitor Resources</a> | <a href="/express/contact/">Contact Us</a>
+							</span>
+					
+					</p><!-- .footer-copyright -->
+						
+
+					</div><!-- .footer-credits -->
+
+					<a class="to-the-top" href="#site-header">
+						<span class="to-the-top-long">
+							<?php
+							/* translators: %s: HTML character for up arrow */
+							printf( __( 'To the top %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+							?>
+						</span><!-- .to-the-top-long -->
+						<span class="to-the-top-short">
+							<?php
+							/* translators: %s: HTML character for up arrow */
+							printf( __( 'Up %s', 'twentytwenty' ), '<span class="arrow" aria-hidden="true">&uarr;</span>' );
+							?>
+						</span><!-- .to-the-top-short -->
+					</a><!-- .to-the-top -->
+
+				</div><!-- .section-inner -->
+
+			</footer><!-- #site-footer -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
---
+++ themes/guestpass-new/templates/template-home.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Template Name: Homepage Template
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since 1.0
+ */
+
+
+get_header();
+?>
+
+<iframe src="https://static.frequency.com/nab-player/index.html?id=6646834083015971138" width="800" height="450" frameborder="no"></iframe>
+
+<main id="site-content" role="main">
+
+	<?php
+
+	if ( have_posts() ) {
+
+		while ( have_posts() ) {
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+		}
+	}
+
+	?>
+	
+	
+
+</main><!-- #site-content -->
+
+
+<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<?php get_footer(); ?>
