e4a0c8e98db67b3fe4d0fec7d1ccf4f2c6795380
jayzerz <53316443+jayzerz@users.noreply.github.com>
2020-07-31 01:55:21 -0400

    Theme Mods

--- themes/getwid-base-child/footer.php
+++ themes/getwid-base-child/footer.php
@@ -23,22 +23,20 @@ get_sidebar();
 	if ( $show_site_info ):
 		?>
 		<div class="site-info">
-			<?php
-			$dateObj           = new DateTime;
-			$current_year      = $dateObj->format( "Y" );
-			$site_info_default = apply_filters( 'getwid_base_site_info',
-				/* translators: %1$s: current year. */
-				esc_html_x( 'Copyright &copy; %1$s.  All Rights Reserved.', 'Default footer text. %1$s - current year.', 'getwid-base' )
-			);
-			$site_info         = get_theme_mod( 'getwid_base_footer_text', false ) ? get_theme_mod( 'getwid_base_footer_text' ) : $site_info_default;
+			<p class="footer-copyright">
+							<span class="footer-related">Related Shows: <a style="color: #000000;" href="https://nabshow.com" target="_blank" rel="noopener noreferrer">NAB Show</a> | <a style="color: #000000;" href="https://nabshowny.com" target="_blank" rel="noopener noreferrer">NAB Show New York</a> | <a style="color: #000000;" href="https://nabshow.com/express/" target="_blank" rel="noopener noreferrer">NAB Show Express</a><br /><br />
+							</span>
 
-			echo wp_kses_post(
-				sprintf(
-					$site_info,
-					$current_year
-				)
-			);
-			?>
+						&copy;
+							<?php
+							echo date_i18n(
+								/* translators: Copyright date format, see https://secure.php.net/date */
+								_x( 'Y', 'copyright date format', 'getwid-base' )
+							);
+							?>
+							<a href="https://nab.org/" target="_blank">National Association of Broadcasters</a>. All Rights Reserved.<br />
+							<span class="footer-related">
+							<a href="/august/privacy-policy/">Privacy Policy</a> | <a href="/august/terms-of-use/">Terms of Use</a> |   <a href="/august/contact/">Contact Us</a>
 		</div>
 	<?php
 	endif;
