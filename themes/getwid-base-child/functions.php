625845e12dadfaac1eaab498349b9a326dabceaf
Faisal Alvi <faisalalvi33@gmail.com>
2020-08-05 17:28:09 +0530

    Added custom disclaimer meta box for events

--- themes/getwid-base-child/functions.php
+++ themes/getwid-base-child/functions.php
@@ -35,3 +35,36 @@ add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 if ( function_exists( 'wpcom_vip_load_gutenberg' ) ) {
     wpcom_vip_load_gutenberg( true );
 }
+
+// Add Custom Meta Box for Disclaimer Text
+function event_desclaimer_text_box() {
+    $screens = ['tribe_events'];
+    foreach ($screens as $screen) {
+        add_meta_box(
+            'event_desclaimer_meta',
+            'Disclaimer Text',
+            'event_desclaimer_text_box_html', 
+            $screen
+        );
+    }
+}
+add_action('add_meta_boxes', 'event_desclaimer_text_box');
+
+function event_desclaimer_text_box_html( $post ) {
+    $value = get_post_meta($post->ID, '_event_desclaimer_text', true);
+
+    $content   = $value;
+    $editor_id = 'event_desclaimer_text_editor'; 
+    wp_editor( $content, $editor_id );
+}
+
+function save_event_desclaimer_text( $post_id ) {
+    if (array_key_exists('event_desclaimer_text_editor', $_POST)) {
+        update_post_meta(
+            $post_id,
+            '_event_desclaimer_text',
+            $_POST['event_desclaimer_text_editor']
+        );
+    }
+}
+add_action('save_post', 'save_event_desclaimer_text');
