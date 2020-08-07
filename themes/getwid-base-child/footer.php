7813bf79c75772d2ac1ea575d08389f4b06d12c5
jayzerz <53316443+jayzerz@users.noreply.github.com>
2020-08-07 03:04:53 -0400

    Uploading Getwid theme and child

---
+++ themes/getwid-base-child/footer.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package getwid_base
+ */
+
+?>
+
+</div><!-- #content -->
+
+<?php
+get_sidebar();
+?>
+<footer id="colophon" class="site-footer">
+	<?php
+
+	$show_site_info = (bool) get_theme_mod( 'getwid_base_show_footer_text', true );
+	if ( $show_site_info ):
+		?>
+		<div class="site-info">
+			<p>
+				<ul class="social-list">
+				<li>
+				<a href="https://www.facebook.com/pages/NAB-Show/195269533220" class="social-fb" target="_blank" rel="noopener noreferrer">
+				<i class="fa fa-facebook"></i>
+				</a>
+				</li>
+				<li>
+				<a href="https://twitter.com/nabshow" class="social-twitter" target="_blank" rel="noopener noreferrer">
+				<i class="fa fa-twitter"></i>
+				</a>
+				</li>
+				<li>
+				<a href="https://www.youtube.com/user/TheNABShow" class="social-youtube" target="_blank" rel="noopener noreferrer">
+				<i class="fa fa-youtube"></i>
+				</a>
+				</li>
+				<li>
+				<a href="https://www.linkedin.com/company/10428507/" class="social-linkedin" target="_blank" rel="noopener noreferrer">
+				<i class="fa fa-linkedin"></i>
+				</a>
+				</li>
+				<li>
+				<a href="http://instagram.com/nabshow?ref=badge" class="social-insta" target="_blank" rel="noopener noreferrer">
+				<i class="fa fa-instagram"></i>
+				</a>
+				</li>
+				</ul>
+			</p>
+			<p class="footer-copyright">
+				<span class="footer-related">Related Shows: <a href="https://nabshow.com" target="_blank" rel="noopener noreferrer">NAB Show</a> | <a href="https://nabshowny.com" target="_blank" rel="noopener noreferrer">NAB Show New York</a> | <a href="https://nabshow.com/express/" target="_blank" rel="noopener noreferrer">NAB Show Express</a><br /><br />
+
+				<br />
+						&copy;
+							<?php
+							echo date_i18n(
+								/* translators: Copyright date format, see https://secure.php.net/date */
+								_x( 'Y', 'copyright date format', 'getwid-base' )
+							);
+							?>
+								<a href="https://nab.org/" target="_blank">National Association of Broadcasters</a> All Rights Reserved.<br /><br />
+								<span class="footer-related">
+								<a href="/privacy-policy/" target="_blank">Privacy Policy</a> | <a href="/terms-of-use/">Terms of Use</a> |   <a href="/contact/">Contact Us</a>
+					</span>
+				</p>
+		</div>
+	<?php
+	endif;
+	?>
+</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php // wp_footer(); ?>
+
+</body></html>
---
+++ themes/getwid-base-child/functions.php
@@ -0,0 +1,85 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array( 'linearicons-free' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
+
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_ext1', trailingslashit( get_theme_root_uri() ) . 'getwid-base-child/Molot-webfont.woff' );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+
+// Load Gutenberg Block Editor on VIP Go for this /*
+
+if ( function_exists( 'wpcom_vip_load_gutenberg' ) ) {
+    wpcom_vip_load_gutenberg( true );
+}
+
+// Add Custom Meta Box for Disclaimer Text
+function event_desclaimer_text_box() {
+    $screens = ['tribe_events'];
+    foreach ($screens as $screen) {
+        add_meta_box(
+            'event_desclaimer_meta',
+            'Disclaimer Text',
+            'event_desclaimer_text_box_html',
+            $screen
+        );
+    }
+}
+add_action('add_meta_boxes', 'event_desclaimer_text_box');
+
+function event_desclaimer_text_box_html( $post ) {
+    $value = get_post_meta($post->ID, '_event_desclaimer_text', true);
+
+    $content   = $value;
+    $editor_id = 'event_desclaimer_text_editor';
+    wp_editor( $content, $editor_id );
+}
+
+function save_event_desclaimer_text( $post_id ) {
+    if (array_key_exists('event_desclaimer_text_editor', $_POST)) {
+        update_post_meta(
+            $post_id,
+            '_event_desclaimer_text',
+            $_POST['event_desclaimer_text_editor']
+        );
+    }
+}
+add_action('save_post', 'save_event_desclaimer_text');
+
+// // Function to change email address
+//
+// function wpb_sender_email( $original_email_address ) {
+//     return 'register@nab.org';
+// }
+//
+// // Function to change sender name
+// function wpb_sender_name( $original_email_from ) {
+//     return 'Collectively Speaking, NAB Show';
+// }
+//
+// // Hooking up our functions to WordPress filters
+// add_filter( 'wp_mail_from', 'wpb_sender_email' );
+// add_filter( 'wp_mail_from_name', 'wpb_sender_name' );
