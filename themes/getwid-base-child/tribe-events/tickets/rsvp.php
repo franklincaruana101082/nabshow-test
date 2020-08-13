1c50ec2fc1f053b49d849bbfdad855ef9ead8b45
jayzerz <53316443+jayzerz@users.noreply.github.com>
2020-08-13 15:03:31 -0400

    Code Mods

--- themes/getwid-base-child/tribe-events/tickets/rsvp.php
+++ themes/getwid-base-child/tribe-events/tickets/rsvp.php
@@ -150,7 +150,8 @@ if ( ! $already_rendered ) {
 							min="0"
 							max="<?php echo esc_attr( $max_at_a_time ); ?>"
 							name="quantity_<?php echo absint( $ticket_id ); ?>"
-							value="0"
+							value="1"
+<!-- value="0" -->
 							<?php disabled( $must_login ); ?>
 						>
 						<?php if ( - 1 !== $available && ( 0 === $threshold || $available <= $threshold ) ) : ?>
