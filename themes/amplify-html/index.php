619d7537fdcd52fc1adf584d2d0845f97cf31c81
Valerie DuBreuil <vdubreuil@nab.org>
2020-08-20 09:45:09 -0400

    Tweaks to tempoary Amplify landing

---
+++ themes/amplify-html/index.php
@@ -0,0 +1,412 @@
+<?php /* Template Name: Amplify Temporary */ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><script type="text/javascript" async="" src="./NAB21OV_files/munchkin.js"></script>
+			
+			<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes">
+			<meta name="apple-mobile-web-app-capable" content="yes"><meta name="robots" content="index, nofollow">
+ 
+     <meta name="description" content="Immerse yourself in a brand new, amplified NAB experience like you’ve never seen.">
+    <meta name="author" content="NAB Amplify">
+
+
+   <title>NAB Amplify</title>
+   
+   <!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-144215902-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-144215902-1');
+	</script>
+	
+		    <style>
+
+				body {
+					background-color:	#404040;
+					color: 				#fff;
+					font-family: 		'Open Sans', sans-serif;
+					font-size: 			16px;
+					margin: 			0;
+					padding: 			0;
+				} 
+
+				body *{
+					box-sizing: 		border-box;
+				}
+
+				#screen{
+					min-height:			100vh;
+					position:			relative;
+					width:				100%;
+				}
+
+				#content{
+					display: 			table;
+					height:				100%;
+					padding: 			50px;
+					position:			relative;
+					width: 				100%;
+					z-index:			100;
+				}
+
+				#content  > *{
+					display: 			table-cell;
+					vertical-align: 	middle;
+				}
+
+
+				#content  > *  > *{
+					display: 			block;
+					margin: 			0 auto;
+					max-width: 			1200px;
+					text-align: 		center;
+				}
+
+				#brand {
+				    margin: 			30px auto;
+				}
+
+
+				#brand img{
+					max-width: 			300px;
+				}
+
+				#content h1{
+					color:				#fdd80f;
+					font-size: 			600%;
+				    line-height: 		1;
+				    margin: 			20px 0 20px 0;
+					z-index:			100;
+				}
+
+				#content p{
+					margin: 			0 0 10px 0;
+				}
+
+				a,
+				a:visited{
+					color:				#fdd80f;
+					transition: 		all 0.8s ease-in-out;
+				}
+
+				a:hover{
+					color:				#e5018b;
+				}
+
+				em{
+					color:				#fdd80f;
+					font-style: 		normal;
+				}
+
+				sup{
+				    line-height: 							0;
+				    position: 								relative;
+					font-size:								20%; 
+				}
+
+				sub{
+				    vertical-align: 						baseline;
+				}
+
+				sup{    
+				    display: 								inline-block;
+					margin-top: 							20px;
+				    vertical-align: 						top;
+				}
+
+				#background,
+				#transparency{
+					position:			absolute;
+					height:				100%;
+					left:				0;
+					top:				0;
+					width:				100%;
+				}
+
+				#transparency{
+					background: 		rgba(0,0,0,0.5);
+					z-index:			50;
+				}
+
+
+				#background{
+					position:			absolute;
+					z-index:			0;
+				}
+
+				#background img{
+				  	height: 			100%;
+				  	object-fit: 		cover;
+				    -o-object-fit: 		cover;
+				    opacity: 			0.4;
+				    top: 				0;
+				    width: 				100%;
+				}
+
+				#marketo-form {    
+					margin: 			40px auto 40px auto;
+				}
+
+				.framing {    
+    				max-width: 			800px;
+					margin-left: 		auto;
+					margin-right: 		auto;
+				}
+
+				#screen form{
+					width:				100% !important;
+				}
+
+				#screen .mktoForm .mktoOffset {
+					width:				0 !important;
+				}
+
+				#screen form .mktoFormRow{
+					display: 			inline-block;
+					width:				50% !important;
+				}
+
+
+				#screen form .mktoFormRow:nth-of-type(3){
+					display: 			inline-block;
+				    margin: 			0px auto;
+					width: 				100% !important;
+				}
+
+				#screen form .mktoFormRow:nth-of-type(3) > *{  
+    				float: 				none;  
+    				margin: 			0 auto;
+    				width: 				50% !important;
+				}
+
+				#screen .mktoButtonRow {
+				    margin: 			0;
+				    text-align: 		center;
+				    width: 				100% !important;
+				}
+
+				#screen form .mktoFormRow > *{
+					padding: 			0 5px;
+				    width: 				100% !important;
+				}
+
+				#screen .mktoForm .mktoFieldWrap {
+				    width: 				100% !important;
+				}
+
+				#screen form label{
+					display: 			none;
+				}
+
+				#screen form .mktoGutter{
+					width:				0 !important;
+				}
+
+				#screen form input{
+					padding: 			10px 17px;
+					width:				100% !important;
+				}
+
+				#screen .mktoForm .mktoButtonWrap.mktoSimple .mktoButton {
+				    background-color: 	#e5018b;
+				    background-image: 	none;
+				    border: 			0;
+				    border-radius: 		8px;
+				    color: 				#fff;
+				    font-size: 			inherit;
+				    padding: 			10px 30px;
+				}
+
+
+				#screen .mktoButtonWrap.mktoSimple{
+					margin-left: 		0 !important;
+				}
+
+				@-webkit-keyframes rotating /* Safari and Chrome */ {
+				  from {
+				    -webkit-transform: rotate(0deg);
+				    -o-transform: rotate(0deg);
+				    transform: rotate(0deg);
+				  }
+				  to {
+				    -webkit-transform: rotate(360deg);
+				    -o-transform: rotate(360deg);
+				    transform: rotate(360deg);
+				  }
+				}
+
+				@keyframes rotating {
+				  from {
+				    -ms-transform: rotate(0deg);
+				    -moz-transform: rotate(0deg);
+				    -webkit-transform: rotate(0deg);
+				    -o-transform: rotate(0deg);
+				    transform: rotate(0deg);
+				  }
+				  to {
+				    -ms-transform: rotate(360deg);
+				    -moz-transform: rotate(360deg);
+				    -webkit-transform: rotate(360deg);
+				    -o-transform: rotate(360deg);
+				    transform: rotate(360deg);
+				  }
+				}
+
+				#brand img {
+				  -webkit-animation: rotating 75s linear infinite;
+				  -moz-animation: rotating 75s linear infinite;
+				  -ms-animation: rotating 75s linear infinite;
+				  -o-animation: rotating 75s linear infinite;
+				  animation: rotating 75s linear infinite;
+				}
+
+
+				@media screen and (max-width: 800px) {
+
+					#content h1 {
+					    font-size: 500%;
+					}
+
+				}
+
+
+				@media screen and (max-width: 630px) {
+
+					#content h1 {
+					    font-size: 400%;
+					}
+
+
+					#screen form .mktoFormRow{
+						display: 			block;
+						width:				100% !important;
+					}
+
+					#screen form .mktoFormRow:nth-of-type(3) > * {
+					    width: 100% !important;
+					}
+
+				}
+
+				@media screen and (max-width: 510px) {
+
+					#content h1 {
+					    font-size: 350%;
+					}
+
+
+				}
+
+				@media screen and (max-width: 410px){
+
+					#content h1 {
+					    font-size: 280%;
+					}
+
+					#brand img {
+					    max-width: 220px;
+					}
+
+				}
+
+		    </style>
+
+		    <link href="./NAB21OV_files/css2" rel="stylesheet">
+	  	<link rel="shortcut icon" href="http://go.nabshow.com/favicon.ico" type="image/x-icon">
+<link rel="icon" href="http://go.nabshow.com/favicon.ico" type="image/x-icon">
+
+<style id="mktoDesktopStyle">
+.lpeCElement {
+  position:absolute;
+}
+.imageSpan img{
+  width:100%; height:auto;
+}
+
+</style>
+
+	  	<link id="mktoForms2BaseStyle" rel="stylesheet" type="text/css" href="./NAB21OV_files/forms2.css"><link id="mktoForms2ThemeStyle" rel="stylesheet" type="text/css" href="./NAB21OV_files/forms2-theme-simple.css"></head>
+
+  		<body id="bodyId">
+  			
+  			<!-- screen -->
+		    <div id="screen" class="mktoContent">
+
+	  			<!-- content -->
+			    <div id="content">
+		    	
+		    		<div class="wrap">
+		    			<div class="inner">
+		    				<div id="brand"><img src="./NAB21OV_files/nab.brand.png"></div>
+			    			<p>Immerse yourself in a brand new, amplified NAB experience like you’ve never seen.</p>
+			    			<h1>NAB Amplify<sup>TM</sup></h1>
+			    			<p><em>Coming this November.</em></p>
+			    			<p>Get on the list to be the first to know more.</p>
+
+			    			<div id="marketo-form" class="framing">
+			    				<script src="./NAB21OV_files/forms2.min(1).js"></script>
+								<form id="mktoForm_1113" novalidate="novalidate" class="mktoForm mktoHasWidth mktoLayoutLeft" style="font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: rgb(51, 51, 51); width: 271px;"><style type="text/css">.mktoForm .mktoButtonWrap.mktoSimple .mktoButton {
+color:#fff;
+border:1px solid #75ae4c;
+padding:0.4em 1em;
+font-size:1em;
+background-color:#99c47c;
+background-image: -webkit-gradient(linear, left top, left bottom, from(#99c47c), to(#75ae4c));
+background-image: -webkit-linear-gradient(top, #99c47c, #75ae4c);
+background-image: -moz-linear-gradient(top, #99c47c, #75ae4c);
+background-image: linear-gradient(to bottom, #99c47c, #75ae4c);
+}
+.mktoForm .mktoButtonWrap.mktoSimple .mktoButton:hover {
+border:1px solid #447f19;
+}
+.mktoForm .mktoButtonWrap.mktoSimple .mktoButton:focus {
+outline:none;
+border:1px solid #447f19;
+}
+.mktoForm .mktoButtonWrap.mktoSimple .mktoButton:active{
+background-color:#75ae4c;
+background-image: -webkit-gradient(linear, left top, left bottom, from(#75ae4c), to(#99c47c));
+background-image: -webkit-linear-gradient(top, #75ae4c, #99c47c);
+background-image: -moz-linear-gradient(top, #75ae4c, #99c47c);
+background-image: linear-gradient(to bottom, #75ae4c, #99c47c);
+}
+</style><div class="mktoFormRow"><div class="mktoFieldDescriptor mktoFormCol" style="margin-bottom: 10px;"><div class="mktoOffset" style="width: 10px;"></div><div class="mktoFieldWrap mktoRequiredField"><label for="FirstName" id="LblFirstName" class="mktoLabel mktoHasWidth" style="width: 100px;"><div class="mktoAsterix">*</div></label><div class="mktoGutter mktoHasWidth" style="width: 10px;"></div><input id="FirstName" name="FirstName" placeholder="first name" maxlength="255" aria-describedby="ValidMsgFirstName" aria-labelledby="LblFirstName InstructFirstName" type="text" class="mktoField mktoTextField mktoHasWidth mktoRequired" aria-required="true" style="width: 150px;"><span id="InstructFirstName" tabindex="-1" class="mktoInstruction"></span><div class="mktoClear"></div></div><div class="mktoClear"></div></div><div class="mktoClear"></div></div><div class="mktoFormRow"><div class="mktoFieldDescriptor mktoFormCol" style="margin-bottom: 10px;"><div class="mktoOffset" style="width: 10px;"></div><div class="mktoFieldWrap mktoRequiredField"><label for="LastName" id="LblLastName" class="mktoLabel mktoHasWidth" style="width: 100px;"><div class="mktoAsterix">*</div></label><div class="mktoGutter mktoHasWidth" style="width: 10px;"></div><input id="LastName" name="LastName" placeholder="last name" maxlength="255" aria-describedby="ValidMsgLastName" aria-labelledby="LblLastName InstructLastName" type="text" class="mktoField mktoTextField mktoHasWidth mktoRequired" aria-required="true" style="width: 150px;"><span id="InstructLastName" tabindex="-1" class="mktoInstruction"></span><div class="mktoClear"></div></div><div class="mktoClear"></div></div><div class="mktoClear"></div></div><div class="mktoFormRow"><div class="mktoFieldDescriptor mktoFormCol" style="margin-bottom: 10px;"><div class="mktoOffset" style="width: 10px;"></div><div class="mktoFieldWrap mktoRequiredField"><label for="Email" id="LblEmail" class="mktoLabel mktoHasWidth" style="width: 100px;"><div class="mktoAsterix">*</div></label><div class="mktoGutter mktoHasWidth" style="width: 10px;"></div><input id="Email" name="Email" placeholder="email" maxlength="255" aria-describedby="ValidMsgEmail" aria-labelledby="LblEmail InstructEmail" type="email" class="mktoField mktoEmailField mktoHasWidth mktoRequired" aria-required="true" style="width: 150px;"><span id="InstructEmail" tabindex="-1" class="mktoInstruction"></span><div class="mktoClear"></div></div><div class="mktoClear"></div></div><div class="mktoClear"></div></div><div class="mktoButtonRow"><span class="mktoButtonWrap mktoSimple" style="margin-left: 120px;"><button type="submit" class="mktoButton">Submit</button></span></div><input type="hidden" name="formid" class="mktoField mktoFieldDescriptor" value="1113"><input type="hidden" name="munchkinId" class="mktoField mktoFieldDescriptor" value="927-ARO-980"></form>
+								<script>MktoForms2.loadForm("//app-ab34.marketo.com", "927-ARO-980", 1113);</script>
+			    			</div>
+			    			<div class="framing">
+			    			<p>To learn more about how Amplify can help technology solution providers, generate leads, elevate brands and drive business opportunities to new levels, contact your account manager at <a href="mailto:exhibit@nab.org">exhibit@nab.org</a></p>
+			    			</div>
+			    		</div>
+		    		</div>
+
+			    </div>
+	    		<!-- content -->
+
+	  			<!-- transparency -->
+			    <div id="transparency">
+		    	
+
+			    </div>
+	    		<!-- transparency -->
+
+	  			<!-- background -->
+			    <div id="background">
+			    	<img src="./NAB21OV_files/spectral.png">
+			    </div>
+	    		<!-- background -->
+
+		    
+
+		    </div>
+  			<!-- screen -->
+	  <script type="text/javascript" src="./NAB21OV_files/munchkin(1).js"></script><script>Munchkin.init('927-ARO-980', {customName: 'NAB21OV', wsInfo: 'j1RR'});</script>
+	  <script type="text/javascript" src="./NAB21OV_files/stripmkttok.js"></script>
+
+<div id="mktoStyleLoaded" style="display: none; border-top-color: rgb(18, 52, 86);"></div><form novalidate="novalidate" class="mktoForm mktoHasWidth mktoLayoutLeft" style="font-family: Helvetica, Arial, sans-serif; font-size: 13px; color: rgb(51, 51, 51); visibility: hidden; position: absolute; top: -500px; left: -1000px; width: 1326px;"></form><iframe name="mktoFormsXDIframe0.6102125884940421" id="MktoForms2XDIframe" src="./NAB21OV_files/XDFrame.html" style="display: none;"></iframe></body></html>
