09589d49eae00064d918cbfc9ca6817cf4c66e4f
Hardik Thakkar <hardik.thakkar@multidots.com>
2020-08-26 16:23:45 +0530

    add header logos

--- themes/nab-amplify/header.php
+++ themes/nab-amplify/header.php
@@ -42,18 +42,7 @@
 							endwhile; ?>
 						</ul>
 					<?php endif; ?>
-					<div class="site-branding">
-						<?php
-						the_custom_logo();
-						if ( is_front_page() && is_home() ) :
-							?>
-							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-						<?php
-						else :
-							?>
-							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-						<?php endif; ?>
-					</div><!-- .site-branding -->
+
 				</div>
 
 				<nav id="site-navigation" class="main-navigation">
