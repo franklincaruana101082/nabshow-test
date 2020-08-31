526c7da96cdce1e2bdc219bac5934e36a83f426b
Clinton Hall <clinton@marketingdesigngroup.com>
2020-08-31 18:29:48 -0400

    [chore] Segment script for Development only include.

--- themes/nabshow-ny/functions.php
+++ themes/nabshow-ny/functions.php
@@ -1,15 +1,5 @@
 <?php
 
-// PARENT STYLE
-
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-
-function enqueue_parent_styles() {
-
-	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', array(), '1.1' );
-
-}
-
 // WAYPOINTS
 
 add_action('wp_enqueue_scripts', 'waypoint_enqueue_script');
@@ -22,15 +12,15 @@ function waypoint_enqueue_script() {
 
 }
 
-// THEME PAGE
+// NY SCRIPT
 
-add_action( 'wp_enqueue_scripts', 'ny_theme_scripts' );
+add_action( 'wp_enqueue_scripts', 'ny_enqueue_styles', PHP_INT_MAX);
 
-function ny_theme_scripts() {
+function ny_enqueue_styles() {
 
-    $css_mod_time = filemtime(get_stylesheet_directory() . '/style.css');
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
-	wp_enqueue_style ( 'style', get_stylesheet_directory_uri() . '/style.css', false, $css_mod_time );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
 
     wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array(), '2.0.0', true );
 
@@ -41,4 +31,7 @@ function ny_theme_scripts() {
 
 }
 
-?>
+/**
+ * Include files for theme customize hooks.
+ */
+require_once get_stylesheet_directory() . '/inc/segment-ga-dev.php';
