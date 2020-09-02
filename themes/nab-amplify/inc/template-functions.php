370b51e16da4bd5ee096c85ffcd69266eedde300
Hardik Thakkar <hardik.thakkar@multidots.com>
2020-09-02 19:33:16 +0530

    email template changes

--- themes/nab-amplify/inc/template-functions.php
+++ themes/nab-amplify/inc/template-functions.php
@@ -9,6 +9,7 @@
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
+ *
  * @return array
  */
 function nab_amplify_body_classes( $classes ) {
@@ -24,6 +25,7 @@ function nab_amplify_body_classes( $classes ) {
 
 	return $classes;
 }
+
 add_filter( 'body_class', 'nab_amplify_body_classes' );
 
 /**
@@ -33,14 +35,14 @@ add_filter( 'body_class', 'nab_amplify_body_classes' );
  */
 function nab_amplify_get_user_images() {
 
-	$user_id   = get_current_user_id();
+	$user_id           = get_current_user_id();
 	$user_images_names = array( 'profile_picture', 'banner_image' );
 
 	$user_images = array();
 	foreach ( $user_images_names as $user_image ) {
 		$user_image_id = get_user_meta( $user_id, $user_image, true );
 
-		$user_images[$user_image] = $user_image_id
+		$user_images[ $user_image ] = $user_image_id
 			? wp_get_attachment_image_src( $user_image_id )[0]
 			: get_template_directory_uri() . '/assets/images/avtar.jpg';
 	}
@@ -56,4 +58,5 @@ function nab_amplify_pingback_header() {
 		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
 	}
 }
+
 add_action( 'wp_head', 'nab_amplify_pingback_header' );
