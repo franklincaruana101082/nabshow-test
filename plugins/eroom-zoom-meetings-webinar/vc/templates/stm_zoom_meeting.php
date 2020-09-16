f66e0ff801a235bb2766ee7b36fc3210b9b4debc
Faisal Alvi <faisalalvi33@gmail.com>
2020-09-16 15:50:19 +0530

    removed zoom plugin

--- plugins/eroom-zoom-meetings-webinar/vc/templates/stm_zoom_meeting.php
+++
@@ -1,6 +0,0 @@
-<?php
-$atts = vc_map_get_attributes($this->getShortcode(), $atts);
-extract( $atts );
-if ( ! empty( $meeting_id ) ) {
-    echo do_shortcode( '[stm_zoom_conference post_id="' . $meeting_id . '"]' );
-}
--- plugins/eroom-zoom-meetings-webinar/vc/templates/stm_zoom_webinar.php
+++
@@ -1,6 +0,0 @@
-<?php
-$atts = vc_map_get_attributes( $this->getShortcode(), $atts );
-extract( $atts );
-if ( ! empty( $webinar_id ) ) {
-    echo do_shortcode( '[stm_zoom_webinar post_id="' . $webinar_id . '"]' );
-}
