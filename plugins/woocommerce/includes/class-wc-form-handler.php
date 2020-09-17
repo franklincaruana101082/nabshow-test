2a1b537f86bf6027b66eb9ac116547c8e690f015
Hardik Thakkar <hardik.thakkar@multidots.com>
2020-09-17 12:13:28 +0530

    debugging

--- plugins/woocommerce/includes/class-wc-form-handler.php
+++ plugins/woocommerce/includes/class-wc-form-handler.php
@@ -579,7 +579,7 @@ class WC_Form_Handler {
 	 */
 	public static function update_cart_action() {
 
-		wp_mail( 'hardik.thakkar@mailinator.com', 'START CART', print_r($_REQUEST,  true));
+		wp_mail( 'hardikthakkar@mailinator.com', 'START CART', print_r($_REQUEST,  true));
 
 		if ( ! ( isset( $_REQUEST['apply_coupon'] ) || isset( $_REQUEST['remove_coupon'] ) || isset( $_REQUEST['remove_item'] ) || isset( $_REQUEST['undo_item'] ) || isset( $_REQUEST['update_cart'] ) || isset( $_REQUEST['proceed'] ) ) ) {
 			return;
--- themes/nab-amplify/inc/action/actions.php
+++ themes/nab-amplify/inc/action/actions.php
@@ -91,7 +91,7 @@ add_action( 'wp_authenticate', 'nab_create_jwt_token', 10, 2 );
 // add_action( 'wp_loaded', 'amplify_apply_coupon_code_from_url', PHP_INT_MAX );
 
 // Add coupon code when a product is added to cart once
-add_action( 'woocommerce_add_to_cart', 'amplify_add_coupon_code_to_cart', PHP_INT_MAX );
+// add_action( 'woocommerce_add_to_cart', 'amplify_add_coupon_code_to_cart', PHP_INT_MAX );
 
 /* Parent-Child add to cart actions start */
 add_action( 'woocommerce_remove_cart_item', 'nab_remove_cocart_item', 10, 2 );
