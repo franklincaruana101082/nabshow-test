50aada850a21e377f289ae02b501c4d5a7dd626c
Hardik Thakkar <hardik.thakkar@multidots.com>
2020-09-19 11:47:13 +0530

    debugging rest api error

--- themes/nabshow-ny/inc/general-functions.php
+++ themes/nabshow-ny/inc/general-functions.php
@@ -42,8 +42,8 @@ function nab_ny_get_cart() {
 	return $cart_qty;
 }
 
-add_action( 'wp_ajax_nab_login_add_cart' , 'nab_login_add_cart_callback' );
-add_action( 'wp_ajax_nopriv_nab_login_add_cart' , 'nab_login_add_cart_callback' );
+add_action( 'wp_ajax_nab_login_add_cart' , 'nab_curl_login_add_cart_callback' );
+add_action( 'wp_ajax_nopriv_nab_login_add_cart' , 'nab_curl_login_add_cart_callback' );
 
 function nab_login_add_cart_callback() {
 
@@ -124,6 +124,112 @@ function nab_login_add_cart_callback() {
     $res['err'] = 1;
     $res['requested_url'] = $api_url;
     $response_body = json_decode( $response['body'], true );
+    $res['message'] = ( isset( $response_body['message'] ) && ! empty( $response_body['message'] ) ) ? $response_body['message'] : 'Something went wrong. Please try again!';
+  }
+
+  wp_send_json( $res, 200 );
+    
+}
+
+
+function nab_curl_login_add_cart_callback() {
+
+  $product_id = filter_input( INPUT_POST, 'product_id' );
+  $cart_key   = filter_input( INPUT_POST, 'cart_key' );
+
+  $args = [];
+  $res  = [];
+
+  if( empty( $product_id ) ) {
+    $res['err'] = 1;
+    $res['message'] = 'Product id not found';
+
+    wp_send_json( $res, 200 );
+  }
+
+  $api_base_url = get_option( 'ep_parent_site_url' );
+
+  if( empty( $api_base_url ) ) {
+    $res['err'] = 1;
+    $res['message'] = 'Parent site URL not found!';
+
+    wp_send_json( $res, 200 );
+  }
+
+  $api_url = $api_base_url . 'wp-json/cocart/v1/add-item/';
+
+  if( is_user_logged_in() ) {
+
+    $user_id    = get_current_user_id();
+
+    $user_token = get_user_meta( $user_id, 'nab_jwt_token', true );
+
+    if( empty( $user_token ) ) {
+      $res['err'] = 1;
+      $res['message'] = 'User token missing! Please sign out and sign in again.';
+  
+      wp_send_json( $res, 200 );
+    }
+
+    $bearer = 'Bearer ' . $user_token;
+    $headers = array(
+      'Accept: application/json',
+      'Content-Type: application/json',
+      'Authorization: ' . $bearer,
+    );
+
+    $api_url = add_query_arg( 'return_cart', 'true', $api_url );
+
+  } else {
+
+    if( empty( $cart_key ) ) {
+      $res['err'] = 1;
+      $res['message'] = 'Cart key missing! Please try again.';
+  
+      wp_send_json( $res, 200 );
+    }
+
+    $headers = array(
+      'Accept: application/json',
+      'Content-Type: application/json',
+    );
+
+    $api_url = add_query_arg( array(
+      'cart_key'    => $cart_key,
+      'return_cart' => 'true',
+    ), $api_url );
+
+  }
+
+  $curl = curl_init();
+
+  $args = json_encode(array(
+    'product_id' => $product_id,
+    'quantity' => 1
+  ));
+
+  curl_setopt_array( $curl, array(
+    CURLOPT_URL => $api_url,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => $args,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTPHEADER => $headers
+  ) );
+
+  $response = curl_exec($curl);
+
+  $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+  curl_close($curl);
+
+  if( 200 === $httpcode ) {
+    $res['err'] = 0;
+    $res['body'] = json_decode( $response, true );
+  } else {
+    $res['err'] = 1;
+    $res['requested_url'] = $api_url;
+    $response_body = json_decode( $response, true );
     $res['message'] = ( isset( $response_body['message'] ) && ! empty( $response_body['message'] ) ) ? $response_body['message'] : 'Something went wrong. Please try again!';
   }
 
