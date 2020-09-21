f4328a9283a8195a231a0a444b349c1765213b25
sadikmultani <hello.sadikmultani@gmail.com>
2020-09-21 18:25:07 +0530

    Added Edit attendee details

--- themes/nab-amplify/woocommerce/myaccount/orders.php
+++ themes/nab-amplify/woocommerce/myaccount/orders.php
@@ -190,15 +190,15 @@ do_action( 'woocommerce_before_account_orders', $has_orders ); ?>
 					<h3>Edit Attendee Details</h3>
 					<table>
 						<tr>
-							<td>First Name</td>
+							<td>First Name:</td>
 							<td><input type="text" value="Ruperta"></td>
 						</tr>
 						<tr>
-							<td>Last Name</td>
+							<td>Last Name:</td>
 							<td><input type="text" value="Horribine"></td>
 						</tr>
 						<tr>
-							<td>Email</td>
+							<td>Email:</td>
 							<td><input type="text" value="rhorribine0@narod.ru"></td>
 						</tr>
 					</table>
