90c3348473ac5e8e0a60088156243b42ca868916
sadikmultani <hello.sadikmultani@gmail.com>
2020-09-21 20:17:23 +0530

    Added Secondary Header html and design

--- themes/nabshow-ny/assets/js/main.js
+++ themes/nabshow-ny/assets/js/main.js
@@ -68,7 +68,7 @@ $(function() {
 			offset: '70%'
 	});
 
-	if ($('.nab-header-secondary').length > 0 && $('body').hasClass('show_sec_menu')) {
+	if ($('.nab-header-secondary').length > 0 && $('body').hasClass('page-id-54656')) {
 		var headerSecondary = $('.nab-header-secondary');
 		var headerMain = $('.site-header');
 		if ($('.admin-bar') > 0) {
