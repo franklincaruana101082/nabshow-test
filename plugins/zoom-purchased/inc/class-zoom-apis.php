514d50d59172346597a4466c8237fcec7f0036c2
Faisal Alvi <faisalalvi33@gmail.com>
2020-09-28 22:50:29 +0530

    development under process of zoom integration plugin

--- plugins/zoom-purchased/inc/class-zoom-apis.php
+++ plugins/zoom-purchased/inc/class-zoom-apis.php
@@ -415,11 +415,12 @@ if ( ! class_exists('Zoom_APIs') ) {
                         foreach ( $ac as $current_post_id => $val ) {
                             $zoom_id = get_post_meta( $current_post_id, 'zoom_id', true );
                             if( $zoom_id ) {
-                                $meeting_ids[$blog_id][$product_id . '_' .$current_post_id ] = get_post_meta( $current_post_id, 'zoom_id', true );
+                                $meeting_ids[$blog_id][$product_id . '_' .$current_post_id ] = $zoom_id;
+
+                                echo 'testing-10';
+                                return;
                             }
 
-                            echo 'testing-09';
-                            return;
 				        }
 
 
