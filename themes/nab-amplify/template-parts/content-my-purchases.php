02d460f249a67c4b36e6957e6dd9920dd1049eb7
Nitish Kaila <kailanitish90@gmail.com>
2020-09-30 18:27:14 +0530

    Implemented My Events and My Passes in the my purchased section

--- themes/nab-amplify/template-parts/content-my-purchases.php
+++ themes/nab-amplify/template-parts/content-my-purchases.php
@@ -8,25 +8,32 @@
 ?>
 <div class="purchased-content-wrapper">
 	<?php
-	$current_user = wp_get_current_user();
+	
+	$current_user = wp_get_current_user();	
 	$product_ids  = array();
-	if ('' !== $current_user->user_email) {
+	
+	if ( 0 !== $current_user->ID ) {		
+		
 		$customer_order_ids = get_posts(array(
 			'numberposts' => -1,
 			'fields'      => 'ids',
-			'meta_key'    => '_billing_email',
-			'meta_value'  => $current_user->user_email,
+			'meta_key'    => '_customer_user',
+			'meta_value'  => $current_user->ID,
 			'post_type'   => wc_get_order_types(),
 			'post_status' => 'wc-completed',
 		));
-		//print_r($customer_order_ids);die();
-		if (is_array($customer_order_ids) && count($customer_order_ids) > 0) {
-			foreach ($customer_order_ids as $customer_order_id) {
-				$order = wc_get_order($customer_order_id);
+		
+		if ( is_array( $customer_order_ids ) && count( $customer_order_ids ) > 0) {
+			
+			foreach ( $customer_order_ids as $customer_order_id ) {
+				
+				$order = wc_get_order( $customer_order_id );
 				$items = $order->get_items();
-				foreach ($items as $item) {
+				
+				foreach ( $items as $item ) {
 
 					$product_id = $item->get_product_id();
+					
 					if ( $product_id && ! in_array( $product_id, $product_ids ) ) {
 						$product_ids[] = $product_id;
 					}
@@ -35,95 +42,122 @@
 		}
 	}
 
-	$content_not_found = 1;
-	$content_total = 0;
-    $items_per_page = 6;
-	if (is_array($product_ids) && count($product_ids) > 0) {
-		$default_image     = get_template_directory_uri() . '/assets/images/avtar.jpg';
-		?>
-		<section class="wp-listing-block wp-listing-search my-purchased-content">
-			<div class="all-sessions">
-				<h3>MY CONTENT</h3>
-                <form method="POST" action="" class="wcbd-zip-form">
+	if ( is_array( $product_ids ) && count( $product_ids ) > 0 ) {
+		
+		$default_image = get_template_directory_uri() . '/assets/images/avtar.jpg';
+
+		$product_ids_regex  = '"' . implode('"|"', $product_ids ) . '"';
+
+		$query_args = array(
+			'post_type'         => 'event-shows',
+			'posts_per_page'    => -1,
+			'post_status'       => 'publish',
+			'meta_key'          => 'wc_pay_per_post_product_ids',
+			'meta_value'        => $product_ids_regex,
+			'meta_compare'      => 'REGEXP',
+			'order_by'			=> 'title',
+			'order'				=> 'ASC'
+		);
+
+		$purchased_events = new WP_Query( $query_args );
+
+		if ( $purchased_events->have_posts() ) {
+
+			?>
+			<section class="wp-listing-block wp-listing-search my-purchased-content shows-list">
+				<div class="all-sessions">
+					<h3>MY EVENTS</h3>                
 					<div class="session-product-list">
 						<div class="wp-info">
 							<?php
-							$shown_content = array();
-							foreach ($product_ids as $product_id) {
+							while ( $purchased_events->have_posts() ) {
+							
+								$purchased_events->the_post();
+								
+								$event_id	= get_the_ID();
+								$event_img	= has_post_thumbnail() ? get_the_post_thumbnail_url() : $default_image;
+								$event_date	= get_field( 'show_date', $event_id );
+								$event_url	= get_field( 'show_url', $event_id );
 
-								$associated_content = maybe_unserialize( get_post_meta( $product_id, '_associated_content', true ) );
-
-								if( $associated_content ) {
-
-									foreach( $associated_content as $blog_id => $ac ) {
-
-										// Connect to new multisite
-										switch_to_blog($blog_id);
-
-										foreach( $ac as $current_post_id => $val ) {
-											if( 0 === $val || in_array( $current_post_id, $shown_content[$blog_id] ) ) {
-												continue;
-											}
-											$shown_content[$blog_id][] = $current_post_id;
-											$content_not_found = 0;
-											$current_post_title     = get_the_title($current_post_id);
-											$current_post_link      = get_the_permalink($current_post_id);
-											$current_post_image     = get_the_post_thumbnail_url($current_post_id);
-											$current_post_image     = $current_post_image ? $current_post_image : $default_image;
-											$current_post_image_css = "background-image: url('$current_post_image');";
-											$template_name = get_post_type( $current_post_id );
-                                            $item_on_page = floor( $content_total / $items_per_page ) + 1;
-                                            $content_total ++;
-                                            $style = $item_on_page > 1 ? 'display: none' : '';
-											?>
-											<div class="wp-summary content_card" data-item="<?php echo esc_attr( $item_on_page ) ?>" style="<?php echo esc_attr( $style ) ?>">
-												<div class="item-inner">
-													<div class="thumbnail">
-														<a href="<?php echo esc_url($current_post_link); ?>" target="_blank">
-															<div class="header_content_card" style="<?php echo esc_attr($current_post_image_css); ?>">
-																<span class="category_content_card button"><?php echo esc_html( $template_name ); ?></span>
-															</div>
-														</a>
-														<div class="title_content_card">
-															<h4>
-																<a href="<?php echo esc_url($current_post_link); ?>" target="_blank"><?php echo esc_html($current_post_title); ?></a>
-															</h4>
-														</div>
-													</div>
-												</div>
+								?>
+								<div class="wp-summary content_card">
+									<div class="item-inner">
+										<div class="thumbnail">											
+											<a href="<?php echo esc_url( $event_url ); ?>" target="_blank">
+												<img src="<?php echo esc_url( $event_img ); ?>" class="purchased-img" alt="event-logo" />
+											</a>
+											<div class="title_content_card">
+												<h4>
+													<a href="<?php echo esc_url( $event_url ); ?>" target="_blank"><?php echo esc_html( get_the_title() ); ?></a>
+												</h4>
+												<span class="event-date"><?php echo esc_html( $event_date ); ?></span>
 											</div>
-											<?php
-										}
-
-										wp_reset_query();
-									}
-								}
-								// Quit multisite connection
-								restore_current_blog();
+										</div>
+									</div>
+								</div>
+								<?php
 							}
 							?>
 						</div>
 					</div>
-				</form>
-                <?php
-                $total_pages = ceil( $content_total / $items_per_page );
-                if( $items_per_page < $content_total ) {
-                    ?>
-                    <div id="purchased-pagination">
-                        <i class="navigate-purchased prev-purchased"></i>
-                        <span id="current-page">1</span> of <span id="page-total"><?php echo esc_html( $total_pages ) ?></span>
-                        <i class="navigate-purchased next-purchased"></i>
-                    </div>
-                <?php } ?>
-			</div>
-		</section>
-	<?php
-	}
-	
-	if( 1 === $content_not_found ) {
+				</div>
+			</section>
+			<?php			
+		}
+		wp_reset_postdata();
+
+		$query_args = array(
+			'post_type'         => 'product',
+			'posts_per_page'    => -1,
+			'post_status'       => 'publish',
+			'post__in'			=> $product_ids,
+			'order_by'			=> 'title',
+			'order'				=> 'ASC'
+		);
+
+		$purchased_passes = new WP_Query( $query_args );
+
+		if ( $purchased_passes->have_posts() ) {
+
+			?>
+			<section class="wp-listing-block wp-listing-search my-purchased-content pass-list">
+				<div class="all-sessions">
+					<h3>My Passes</h3>                
+					<div class="session-product-list">
+						<div class="wp-info">
+							<?php
+							while ( $purchased_passes->have_posts() ) {
+							
+								$purchased_passes->the_post();
+																
+								$product_img 	= has_post_thumbnail() ? get_the_post_thumbnail_url() : $default_image;
+								$product_link	= get_the_permalink();
+
+								?>
+								<div class="wp-summary content_card">
+									<div class="item-inner">
+										<div class="thumbnail">											
+											<a href="<?php echo esc_url( $product_link ); ?>" target="_blank">
+												<img src="<?php echo esc_url( $product_img ); ?>" class="purchased-img" alt="pass-logo" />
+											</a>											
+										</div>
+									</div>
+								</div>
+								<?php
+							}
+							?>
+						</div>
+					</div>
+				</div>
+			</section>
+			<?php			
+		}
+		wp_reset_postdata();
+		
+	} else {
 	?>
 		<div class="woocommerce-message woocommerce-message--info woocommerce-Message woocommerce-Message--info woocommerce-info">
-			<a class="woocommerce-Button button" href="<?php echo esc_url(get_site_url() . '/sessions/'); ?>"> Browse content</a>
+			<a class="woocommerce-Button button" href="<?php echo esc_url( wc_get_page_permalink( 'shop' ) ); ?>"> Browse content</a>
 			No purchased content found.
 		</div>
 	<?php
