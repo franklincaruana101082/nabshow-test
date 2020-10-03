a53435ebb84b2dda5cd219ead265a907c803a477
Hardik Thakkar <hardik.thakkar@multidots.com>
2020-10-03 16:06:56 +0530

    checking bulk order issue

--- themes/nab-amplify/test.php
+++ themes/nab-amplify/test.php
@@ -10,7 +10,9 @@ global $wpdb;
 $order_id = 3900;
 
 
-$q = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}nab_attendee WHERE order_id = %d AND status = 1", $order_id ) , ARRAY_A);
+$d = $wpdb->query( $wpdb->prepare( "DELETE FROM {$wpdb->prefix}nab_attendee WHERE order_id = %d AND status = 0", $order_id ) );
+
+$q = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}nab_attendee WHERE order_id = %d", $order_id ) , ARRAY_A);
 
 
 echo '<pre>';
