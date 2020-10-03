fb113da1b94956c6cf8395ebf889f06aac9dc785
Hardik Thakkar <hardik.thakkar@multidots.com>
2020-10-03 12:26:33 +0530

    checking bulk order issue

---
+++ themes/nab-amplify/test.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Template Name: Testing
+ */
+
+get_header();
+
+global $wpdb;
+
+$order_id = 3900;
+
+
+$q = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}nab_attendee WHERE order_id = %d", $order_id ) , ARRAY_A);
+
+
+echo '<pre>';
+print_r( $q );
+echo '</pre>';
+
+get_footer(); 
