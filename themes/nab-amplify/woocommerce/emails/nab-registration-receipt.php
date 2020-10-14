131697e0bb04f1b30c5ee01a50636e0af5e64f25
Valerie DuBreuil <vdubreuil@nab.org>
2020-10-14 14:30:15 -0400

    Updating emails

--- themes/nab-amplify/woocommerce/emails/nab-registration-receipt.php
+++ themes/nab-amplify/woocommerce/emails/nab-registration-receipt.php
@@ -5,15 +5,8 @@ do_action( 'woocommerce_email_header', $email_heading, $email );
 
 ?>
 	<p><?php printf( esc_html__( 'Dear %1s %2s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ), esc_html( $order->get_billing_last_name() ) ); ?></p>
-<p>Thank you for registering for an account on  <?php esc_html_e( 'NAB Amplify.', 'nab-amplify' ); ?><sup>TM</sup>.</p>
-	<p>Today this is your hub for event registration for NAB Show New York<sup>®</sup>, Radio Show and SMTE. But in 2021 this will be your destination to connect with the global media and entertainment community.  
-</p><p>
-NAB Amplify<sup>TM</sup> is building a dynamic platform with YOU at the center of the design. Through personal customization and unique networking opportunities, we want to connect you to people, products and information that is most meaningful to your success. Live shared learning, access to experts for Q&A, live demos and product unveilings and the ability to present your qualifications and interests to a curated community are just a sampling of the experiences you can expect in 2021.  
-</p><p>
-This is also an experience shaped by the community; therefore, we are looking for beta testers to provide input in the early stages of development starting this November. Your feedback is valuable to this process and if you would like to participate, we would appreciate your time and insights.   
-</p><p>Sign up here to contribute as an NAB Amplify<sup>TM</sup> beta tester and we will be in touch soon with more details on how you can test new features. https://amplify.nabshow.com/  
-<p>We look forward to connecting with you virtually. If there is anything, we can help with in the meantime please let us know <a href="mailto:register@nab.org">register@nab.org</a>.
-	</p>	
+<p>Thank you for registering!</p>
+	<p>We look forward to hosting you and other professionals from the media and entertainment industry this October.</p>
 	<p>To view and/or add items to your registration or make changes to your NAB Amplify<sup>TM</sup> account, simply <a
 			href="<?php echo esc_url( wc_get_account_endpoint_url( 'edit-my-profile' ) ); ?>">Log In</a>.</p>
 
