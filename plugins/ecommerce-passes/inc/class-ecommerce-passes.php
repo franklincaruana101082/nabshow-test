af119884a2b49f49b6f5e2ed32661ba9b92ade7a
Nitish Kaila <kailanitish90@gmail.com>
2020-10-20 17:47:13 +0530

    Added missing close tag

--- plugins/ecommerce-passes/inc/class-ecommerce-passes.php
+++ plugins/ecommerce-passes/inc/class-ecommerce-passes.php
@@ -314,7 +314,7 @@ if ( ! class_exists('Ecommerce_Passes') ) {
         public function ep_get_restrict_content( $content, $link = '', $logged_in = false) {
 
             $prodcut_name       = ! empty( $link ) ? '<a class="amplifyGuestSignIn" target="_blank" href="' . $link . '">this program</a>' : 'this program';
-            $restrict_content   = '<p class="restrict-msg">This content is open to registered users only. <a class="amplifyGuestSignIn" href="https://amplify.nabshow.com/my-account/">Sign in</a> or <a target="_blank" class="amplifyGuestSignIn" href="https://amplify.nabshow.com/shop/">register now</a> for access.';
+            $restrict_content   = '<p class="restrict-msg">This content is open to registered users only. <a class="amplifyGuestSignIn" href="https://amplify.nabshow.com/my-account/">Sign in</a> or <a target="_blank" class="amplifyGuestSignIn" href="https://amplify.nabshow.com/shop/">register now</a> for access.</p>';
 
             if ( ! $logged_in ) {
                 $restrict_content .= '';
