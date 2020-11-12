bf8d2bbe5882a8b277a277b935c57b522a150872
sadikmultani <hello.sadikmultani@gmail.com>
2020-11-12 18:54:59 +0530

    block update

--- themes/nab-amplify/blocks/block.js
+++ themes/nab-amplify/blocks/block.js
@@ -6,4 +6,4 @@ import './block/upcoming-events-calendar/block';
 import './block/related-content/block';
 import './block/related-content-2/block';
 import './block/community-curator/block';
-import "./block/tabs/block"
+import "./block/tabs/block";
