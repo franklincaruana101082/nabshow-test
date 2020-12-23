05a647f3d81d30ccf61063766e5179996cbbdd86
sadikmultani <hello.sadikmultani@gmail.com>
2020-12-23 15:24:37 +0530

    removed extra spacing class from popups

--- themes/nab-amplify/inc/nab-edit-company-about.php
+++ themes/nab-amplify/inc/nab-edit-company-about.php
@@ -12,17 +12,17 @@
 								<div class="form-row">
 									<label for="">About</label>
 									<span class="info-msg"><span id="character-count-comp-about">2000 Characters Remaining</span></span>
-									<textarea class="input-text add-product" name="company_about" id="company_about"><?php echo isset($company_data['company_about']) ? $company_data['company_about'] : ''; ?></textarea>
+									<textarea class="input-text" name="company_about" id="company_about"><?php echo isset($company_data['company_about']) ? $company_data['company_about'] : ''; ?></textarea>
 								</div>
 							</div>
 							<div class="company-info-row">
 								<div class="form-row">
 									<label for="">Company Tagline</label>
-									<input type="text" class="input-text add-product" name="company_industry" value="<?php echo isset($company_data['company_industry']) ? $company_data['company_industry'] : ''; ?>" id="company_industry">
+									<input type="text" class="input-text" name="company_industry" value="<?php echo isset($company_data['company_industry']) ? $company_data['company_industry'] : ''; ?>" id="company_industry">
 								</div>
 								<div class="form-row">
 									<label for="">Website</label>
-									<input type="text" class="input-text add-product" name="company_website" value="<?php echo isset($company_data['company_website']) ? $company_data['company_website'] : ''; ?>" id="company_website">
+									<input type="text" class="input-text" name="company_website" value="<?php echo isset($company_data['company_website']) ? $company_data['company_website'] : ''; ?>" id="company_website">
 								</div>
 								<div class="form-row">
 									<label for="">Point of Contact</label>
@@ -55,7 +55,7 @@
 								</div>
 								<div class="form-row">
 									<label for="">Youtube</label>
-									<input type="text" class="input-text add-product" name="company_youtube" value="<?php echo isset($company_data['company_youtube']) ? $company_data['company_youtube'] : ''; ?>" id="company_youtube">
+									<input type="text" class="input-text" name="company_youtube" value="<?php echo isset($company_data['company_youtube']) ? $company_data['company_youtube'] : ''; ?>" id="company_youtube">
 								</div>
 							</div>
 
