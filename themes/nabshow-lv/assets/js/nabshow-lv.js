295f23bdeb56215e3a498eea3021bb1aa10768f9
Nitish Kaila <kailanitish90@gmail.com>
2020-12-31 17:49:04 +0530

    change jquery load method

--- themes/nabshow-lv/assets/js/nabshow-lv.js
+++ themes/nabshow-lv/assets/js/nabshow-lv.js
@@ -98,10 +98,8 @@
   if (0 < $('.nab-interadv-block').length) {
     checkCookie();
   }
-
-
-  $(window).load(function () {
-
+    
+  $(window).on('load', function(){
     /**
      * Startup Loft forms datepicker
      */
@@ -1111,7 +1109,7 @@
       nabAjaxForBrowseSession(sessionItem, 'browse-filter', pageNumber, postStartWith, sessionTrack, sessionLocation, listingType, sessionDate, featuredSession);
     });
     if (0 < $('.browse-sessions-filter #session-date').length || 0 < $('.browse-open-to-all-filter #session-date').length) {
-      $(window).load(function () {
+      $(window).on('load', function(){      
         $('.browse-sessions-filter #session-date, .browse-open-to-all-filter #session-date').datepicker({ dateFormat: 'DD, MM d, yy' }).on('change', function () {
           pageNumber = 1;
           sessionDate = $(this).val();
@@ -1441,7 +1439,7 @@
       nabFilterDestinationPagesHandler(pageLocation, pageType, newThisYear, pageStartWith, pageSearchTitle, featuredPage);
     });
     if (0 < $('.browse-happenings-filter #happenings_date').length) {
-      $(window).load(function () {
+      $(window).on('load', function () {
         $('.browse-happenings-filter #happenings_date').datepicker({ dateFormat: 'DD, MM d, yy' }).on('change', function () {
           nabFilterDestinationPagesHandler(pageLocation, pageType, newThisYear, pageStartWith, pageSearchTitle, featuredPage);
         });
