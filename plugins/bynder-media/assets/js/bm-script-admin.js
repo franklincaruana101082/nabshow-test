a44abaa67136dd3f9e3430c3ff10ffb9f414c89e
Faisal Alvi <faisalalvi33@gmail.com>
2021-02-01 14:51:04 +0530

    added new custom plugins

---
+++ plugins/bynder-media/assets/js/bm-script-admin.js
@@ -0,0 +1,171 @@
+(function ($) {
+
+    // Ready.
+    $(document).ready(function () {
+        addBMpopup();
+    });
+
+    $(document).on('click', '.bm-modal-close', function(){
+        $(this).parents('.bm-modal-main').removeClass('bm-modal-active');
+        $('.bm-select-media').removeClass('active');
+    });
+
+    $(document).on('click', '#bm-featured-remove', function(){
+        // Remove the selected image src in hidden meta field.
+        $('#bm_meta_featured_image').val('');
+
+        // Remove the selected image src in hidden image.
+        $('#bm_meta_featured_image_src').attr('src', '');
+
+        // Replace the link into a select area.
+        $('#bm_meta_featured_outer').removeClass('selected');
+
+        // Change the label name.
+        $('#bm-featured-image').text('Set Bynder Image');
+    });
+
+    $(document).on('click', '.bm-select-media', function () {
+
+        // Flush the popup.
+        $('#bm-main-outer #bm-tab-media').html('');
+
+        // Prevent simultaneous requests.
+        if( 0 !== $('.bm-select-media.active').length ) {
+            alert('Loading... Please wait! Please contact Plugin Developer if you see this message repeatedly.');
+        }
+
+        // Add active class to prevent simultaneous requests.
+        $(this).addClass('active');
+
+        // Add clicked element's ID in #bm-tab-media
+        // to perform respective actions later.
+        const requestedBy = $(this).attr('id');
+        $('#bm-tab-media').attr('bynder-request-by', requestedBy);
+
+        $("#bm-main-outer").addClass('bm-modal-active');
+        $("#bm-main-outer .bm-modal-body").addClass('bm-loading');
+
+        const bmData = new FormData();
+        bmData.append('action', 'bm_fetch_assets');
+        bmData.append('requestedBy', requestedBy);
+
+        $.ajax({
+            type: 'POST',
+            url: bmObj.ajaxurl,
+            data: bmData,
+            contentType: false,
+            processData: false,
+            success(result) {
+                result = JSON.parse(result);
+                if( result.bmHTML ) {
+                    $('#bm-main-outer .bm-modal-body').removeClass('bm-loading');
+                    if( 0 === $('#bm-main-outer').length ) {
+                        addBMpopup();
+                    }
+                    $('#bm-main-outer #bm-tab-media').html(result.bmHTML);
+                } else if ( result.error ) {
+                    alert(result.error);
+                }
+            },
+            error() {
+                alert('Fetch error! Try again or contact Plugin Developer.');
+            },
+        });
+    });
+
+    $(document).on('click', '#bm-tab-media .bm-btn', function() {
+
+        // Get values.
+        const assetCard = $(this).parents('.bm-item');
+        const requestedBy = $('#bm-tab-media').attr('bynder-request-by');
+        const assetName = assetCard.find('.bm-img-name').text();
+        let assetSrc = assetCard.find('[data-name]:checked').val();
+
+        // Check the Featured derivative if the request is for the featured image.
+        if( 'bm-featured-image' === requestedBy ) {
+            assetCard.find('[data-name="Featured"]').prop('checked',true);
+            assetSrc = assetCard.find('[data-name]:checked').val();
+        }
+
+        // Check if the derivative is selected.
+        if (undefined === assetSrc ) {
+            alert('Please select derivative.');
+            return false;
+        }
+
+        // Add the selected image src in hidden meta field.
+        if( 'bm-featured-image' === requestedBy ) {
+            // Add the selected image src in hidden meta field.
+            $('#bm_meta_featured_image').val(assetSrc);
+
+            // Add the selected image src in hidden image.
+            $('#bm_meta_featured_image_src').attr('src', assetSrc);
+
+            // Replace the select area into a link.
+            $('#bm_meta_featured_outer').addClass('selected');
+
+            // Change the label name.
+            $('#bm-featured-image').text('Change Bynder Image');
+
+        } else if ( 'bm-block-image' === requestedBy ) {
+
+            // Replace the bynder block with default image div.
+            const bynderReadyBlock = wp.blocks.createBlock('core/image', {
+                url: assetSrc,
+                alt: assetName
+            });
+            const blockID = $('.bm-select-media.active').closest('[data-block]').attr('data-block');
+            wp.data.dispatch('core/block-editor').replaceBlocks( blockID, bynderReadyBlock );
+        }
+
+        // Close the popup.
+        $('.bm-modal-close').trigger('click');
+    });
+
+    $(document).on('click', '.bm-tab-list .bm-tab-item', function(){
+        var tabID = $(this).attr('data-tab');
+        $(this).parent().addClass('bm-tab-active').siblings().removeClass('bm-tab-active');
+        $('#'+tabID).addClass('bm-tab-active').siblings().removeClass('bm-tab-active');
+    });
+
+    $(document).on('click touchstart', function (e) {
+      if ($(e.target).is('.bm-modal-overlay')) {
+        var checkPopup = $(e.target).parents('.bm-modal-main');
+        if (checkPopup.hasClass('bm-modal-active')) {
+          checkPopup.removeClass('bm-modal-active');
+            $('.bm-select-media').removeClass('active');
+        }
+      }
+    });
+
+})(jQuery);
+
+
+function addBMpopup() {
+
+    if( 0 === jQuery('#bm-main-outer').length ) {
+
+        jQuery('.bm-select-media').addClass('disabled').text('Loading...');
+
+        const bmData = new FormData();
+        bmData.append('action', 'bm_init_popup');
+
+        jQuery.ajax({
+            type: 'POST',
+            url: bmObj.ajaxurl,
+            data: bmData,
+            contentType: false,
+            processData: false,
+            success(result) {
+                result = JSON.parse(result);
+                if( result.bmInitPop ) {
+                    jQuery('body').append(result.bmInitPop);
+                    jQuery('.bm-select-media').removeClass('disabled').text('Select Bynder Image');
+                }
+            },
+            error() {
+                alert('Fetch error! Try again or contact Plugin Developer.');
+            },
+        });
+    }
+}
---
+++ plugins/bynder-media/includes/class-bynder-media.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Bynder Media's Main Plugin Class File.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+if ( ! class_exists( 'Bynder_Media' ) ) {
+
+	class Bynder_Media {
+
+		/**
+		 * Main Ignition Hook.
+		 */
+		public function bm_init_hook() {
+
+			// Action to add script and style in admin area.
+			add_action( 'admin_enqueue_scripts', array( $this, 'bm_enqueue_script_admin' ) );
+
+			// Action for add setting page
+			add_action( 'admin_menu', array( $this, 'bm_add_setting_page' ) );
+
+			// Include required clasess.
+			$this->bm_included_classes();
+		}
+
+		public function bm_included_classes() {
+
+			// Add class for custom metas.
+			require_once BYNDER_MEDIA_DIR . '/includes/classes/class-bm-metas.php';
+
+			// Add class for custom metas.
+			require_once BYNDER_MEDIA_DIR . 'includes/classes/class-bm-ajax.php';
+
+			// Add class for gutenberg blocks.
+			require_once BYNDER_MEDIA_DIR . 'includes/classes/class-bm-blocks.php';
+		}
+
+		/**
+		 * Add Bynder Setting Page.
+		 *
+		 * @since 1.0.0
+		 */
+		public function bm_add_setting_page() {
+
+			$settings_title = 'Bynder Media Settings';
+
+			add_submenu_page(
+				'options-general.php',
+				__( esc_html( $settings_title ), 'bynder-media' ),
+				__( esc_html( $settings_title ), 'bynder-media' ),
+				'manage_options',
+				'bm_custom_settings',
+				array( $this, 'bm_settings_callback' )
+			);
+		}
+
+		/**
+		 * Setting page callback function.
+		 *
+		 * @since 1.0.0
+		 */
+		public function bm_settings_callback() {
+
+			$bm_token = filter_input( INPUT_POST, 'bm_token', FILTER_SANITIZE_STRING );
+			$bm_domain = filter_input( INPUT_POST, 'bm_domain', FILTER_SANITIZE_STRING );
+
+			if ( isset( $bm_token ) && ! empty( $bm_token ) ) {
+				update_option( 'bm_token', $bm_token );
+			}
+			if ( isset( $bm_domain ) && ! empty( $bm_domain ) ) {
+				update_option( 'bm_domain', $bm_domain );
+			}
+			?>
+            <div class="mp-product-settings">
+                <h2>Bynder Media Settings</h2>
+                <form class="bm-settings-form" method="post">
+                    <table class="form-table" role="presentation">
+                        <tr>
+                            <th>
+                                <label for="bm_domain">Bynder Domain:</label>
+                            </th>
+                            <td>
+                                <input type="text" name="bm_domain" id="bm_domain" value="<?php echo esc_attr( get_option( 'bm_domain' ) ); ?>" class="regular-text" required/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>
+                                <label for="bm_token">Bynder Permanent Token:</label>
+                            </th>
+                            <td>
+                                <input type="text" name="bm_token" id="bm_token" value="<?php echo esc_attr( get_option( 'bm_token' ) ); ?>" class="regular-text" required/>
+                            </td>
+                        </tr>
+                    </table>
+					<?php submit_button( "Save" ); ?>
+                </form>
+            </div>
+			<?php
+		}
+
+		/*
+		 * Enqueue script and style at front side.
+		 *
+		 * @param int $hook Hook suffix for the current admin page.
+		 *
+		 * @since 1.0.0
+		 */
+		public function bm_enqueue_script_admin( $hook ) {
+
+
+			// Enable only for..
+			if ( 'post.php' !== $hook && 'post-new.php' !== $hook ) {
+				return;
+			}
+
+			// Add styles.
+			wp_enqueue_style( 'bm-style', BYNDER_MEDIA_URL . 'assets/css/bm-style-admin.css', array(), '1.0.0' );
+
+			// Add scripts.
+			wp_enqueue_script( 'bm-script', BYNDER_MEDIA_URL . 'assets/js/bm-script-admin.js', array( 'jquery' ), '1.0.0' );
+			wp_localize_script( 'bm-script', 'bmObj', array(
+				'ajaxurl' => admin_url( 'admin-ajax.php' ),
+			) );
+		}
+	}
+}
