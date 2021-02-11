8729b91744bde24fb041d4e1ab4d3e89773716e0
Nitish Kaila <kailanitish90@gmail.com>
2021-02-11 18:10:31 +0530

    Added user last activity update script to list user in the buddypress

--- themes/nab-amplify/user-script-template.php
+++ themes/nab-amplify/user-script-template.php
@@ -5,40 +5,17 @@
 
 get_header();
 
-$user_query = new WP_User_Query( array( 'meta_key' => 'attendee_location' ) );
+global $wpdb;
 
+$user_query = new WP_User_Query(  array('orderby' => 'login') );
 
 $all_users 	= $user_query->get_results();
-
-?>
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Location</th>
-        <th>Login</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-    </tr>
-<?php
+$cnt = 0;
 
 foreach ( $all_users as $current_user ) {
-    
-    $attendee_location  = get_user_meta( $current_user->ID, 'attendee_location', true );
-    $first_name         = get_user_meta( $current_user->ID, 'first_name', true );
-    $last_name          = get_user_meta( $current_user->ID, 'last_name', true );
-    ?>
-    <tr>
-        <td><?php echo esc_html( $current_user->ID ); ?></td>
-        <td><?php echo esc_html( $attendee_location ); ?></td>
-        <td><?php echo esc_html( $current_user->user_login ); ?></td>
-        <td><?php echo esc_html( $first_name ); ?></td>
-        <td><?php echo esc_html( $last_name ); ?></td>
-    </tr>
-    <?php
+    bp_update_user_last_activity( $current_user->ID );
+    $cnt++;
 }
-?>
-</table>
-<?php
+echo "Total user update = " . $cnt;
 get_footer();
 ?>
-
