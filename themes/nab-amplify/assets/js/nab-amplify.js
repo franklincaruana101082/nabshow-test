d043b79f4d34766263de70dc27a72832d90a8447
shahid <shahid.ajmeri@multidots.com>
2021-02-15 18:04:19 +0530

    fixed website empty field validation

--- themes/nab-amplify/assets/js/nab-amplify.js
+++ themes/nab-amplify/assets/js/nab-amplify.js
@@ -321,9 +321,12 @@
   }
 
   function validateURL (urltext) {
-    
+    if(urltext !== ''){
     var rg = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/
     return rg.test(urltext);
+    }else{
+      return true
+    }
   }
 
   function load_tinyMCE_withPlugins (tag, countTag, limit = 2000) {
