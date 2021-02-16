95b63684b75d757ecb9b816cdfeb854a958ce7ff
shahid <shahid.ajmeri@multidots.com>
2021-02-16 18:04:14 +0530

    *

--- themes/nab-amplify/inc/action/blocks-functions.php
+++ themes/nab-amplify/inc/action/blocks-functions.php
@@ -911,7 +911,7 @@ function nab_company_feature_render_callback($attributes)
     $feature_button_target = get_field('feature_button_target');
     $user_id            = get_current_user_id();
     $admin_id           = get_field('company_user_id', get_the_ID());
-    $member_level = get_field('member_level');
+   echo $member_level = get_field('member_level');
 
     if (is_array($feature_enable_reaction)) {
         $feature_enable_reaction = $feature_enable_reaction[0];
