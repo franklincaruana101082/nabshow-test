a1a00bbbccfc03be0496c804d0c8f5e4878bc2e4
Gudmundur Haraldsson <gdh1@hi.is>
2021-02-17 17:46:40 +0000

    Fix

--- themes/nab-amplify/functions.php
+++ themes/nab-amplify/functions.php
@@ -253,7 +253,7 @@ remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 )
 
 /* Load Gutenberg in custom post types */
 function maybe_load_gutenberg_for_post_type( $can_edit, $post ) {
-	$enable_for_post_types = [ 'page', 'articles', 'wp_block''page', 'articles', 'wp_block', 'company', 'company-products', 'landing-page' ];
+	$enable_for_post_types = [ 'page', 'articles', 'wp_block', 'page', 'articles', 'wp_block', 'company', 'company-products', 'landing-page' ];
 
 	if ( in_array( $post->post_type, $enable_for_post_types, true ) ) {
 		return true;
