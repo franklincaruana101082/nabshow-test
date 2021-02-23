768af5f5e159a836ca1af7aa6fbf03c0ffd27ee8
Matt Spaanem <spaanem@gmail.com>
2021-02-22 16:41:25 -0800

    article layout fix

--- themes/nab-amplify/single-articles.php
+++ themes/nab-amplify/single-articles.php
@@ -11,7 +11,6 @@ get_header();
 ?>
 
 	<!-- START legacy-template: single-articles -->
-	<div class="container">        
 	<main id="primary" class="site-main single_php">
         <div class="nab-ad-block header_ad">
             <broadstreet-zone zone-id="82835"></broadstreet-zone>
@@ -37,7 +36,6 @@ get_header();
             <broadstreet-zone zone-id="82836"></broadstreet-zone>
         </div>
 	</main><!-- #main -->
-	</div><!-- .container -->
 	<!-- END legacy-template -->
 <?php
 get_footer();
