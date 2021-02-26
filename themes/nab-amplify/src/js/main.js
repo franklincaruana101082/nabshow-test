9e2c8d76207d6e40a92bcb2f2031a181ebc2734c
Matt Spaanem <spaanem@gmail.com>
2021-02-26 13:09:03 -0800

    do empty ad check after braodstreet is loaded (in theory)

--- themes/nab-amplify/src/js/main.js
+++ themes/nab-amplify/src/js/main.js
@@ -47,16 +47,20 @@ jQuery(function($) {
 		adaptiveHeight: true
 	});
 
-	jQuery('broadstreet-zone-container, .ad').each(function() {
-		var self = jQuery(this);
-		var adContainer = self.find('broadstreet-zone');
-		var adContent = '';
-		if(adContainer.length) {
-	 		adContent = adContainer.children('div').html().trim();
-	 	}
-		if(adContent=='') {
-			jQuery(self).addClass('_hidden');
-		}
+	document.addEventListener("broadstreetLoaded", function() {
+		jQuery('broadstreet-zone-container, .ad').each(function() {
+			var self = jQuery(this);
+			var adContainer = self.find('broadstreet-zone');
+			var adContent = '';
+			console.log(adContainer.length);
+			if(adContainer.length) {
+		 		adContent = adContainer.children('div').html().trim();
+		 		console.log(adContent);
+		 	}
+			if(adContent=='') {
+				jQuery(self).addClass('_hidden');
+			}
+		});
 	});
 	
 });
