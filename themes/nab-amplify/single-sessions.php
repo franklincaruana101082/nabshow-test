7b57205125ff5badd06de887a6fcbcdcbc6b4e4b
Matt Spaanem <spaanem@gmail.com>
2021-03-01 15:55:31 -0800

    semicolons

--- themes/nab-amplify/single-sessions.php
+++ themes/nab-amplify/single-sessions.php
@@ -145,9 +145,9 @@ get_header();
 								</div>
 								<div class="embed-wrapper _chat">
 									<?php 
-										$cometchat_shortcode = "[cometchat-pro widget-id='" .$chat_room_id."' widget-version='v2']"
-										//do_shortcode($cometchat_shortcode); 
-										echo("<!--".$cometchat_shortcode."-->");
+										$cometchat_shortcode = "[cometchat-pro widget-id='" .$chat_room_id."' widget-version='v2']";
+										echo do_shortcode($cometchat_shortcode); 
+										//echo("<!--".$cometchat_shortcode."-->");
 									?>
 								</div>
 								</div>
