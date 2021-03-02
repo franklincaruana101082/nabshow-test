4b935b69c0d64f214dfb262c2058a8a4c7ad1f92
Faisal Alvi <faisalalvi33@gmail.com>
2021-03-02 13:40:34 +0530

    Bynder debug

--- plugins/bynder-media/includes/classes/class-bm-ajax.php
+++ plugins/bynder-media/includes/classes/class-bm-ajax.php
@@ -195,8 +195,17 @@ if ( ! class_exists( 'Bynder_Media_Ajax' ) ) {
 					// Get uploaded URL.
 					$this->args['image_url'] = wp_get_original_image_path( $attachment_id );
 
+
+					$test1 = ABSPATH;
+					$this->args['image_url'] = ABSPATH . 'wp-content/uploads/sites/9/2021/03/0_ReDn_LN2rtXno-W5-1.png';
+
 					// Set name to upload.
 					$this->args['image_name'] = $_FILES["croppedImage"]['name'];
+
+					echo '<pre>';
+					print_r( get_defined_vars() );
+					print_r( $this );
+					die( '<br><---died here' );
 
 					// Init upload.
 					require_once( BYNDER_MEDIA_DIR . 'includes/partials/bm-sdk-upload-asset.php' );
