3fae53d04451b118405079d350b96f8bec49f444
Matt Spaanem <spaanem@gmail.com>
2021-03-03 08:26:40 -0800

    fix nabcard class names

--- themes/nab-amplify/archive-sessions.php
+++ themes/nab-amplify/archive-sessions.php
@@ -70,14 +70,14 @@ $sessions = get_posts( array(
 
 				<?php 
 				if($i === 0) : ?>
-				<div class="card">
-					<div class="card__content">
+				<div class="nabcard">
+					<div class="nabcard__content">
 						<ul class="events__featured">
 				<?php endif; ?>
 				<?php 
 				if($i === 3) : ?>
-				<div class="card">
-					<div class="card__content">
+				<div class="nabcard">
+					<div class="nabcard__content">
 						<ul class="events__list">
 				<?php endif; ?>
 							<li>
@@ -124,14 +124,14 @@ $sessions = get_posts( array(
 				<?php 
 				if($i === 2) : ?>
 						</ul> <!--.events__featured -->
-					</div> <!--.card__content -->
-				</div> <!--.card -->
+					</div> <!--.nabcard__content -->
+				</div> <!--.nabcard -->
 				(INSERT AD)
 				<?php endif; ?>
 				<?php if ($i == $last_event - 1): ?>
 						</ul><!--.events__list-->
-					</div><!--.card__content-->
-				</div><!--.card-->
+					</div><!--.nabcard__content-->
+				</div><!--.nabcard-->
 				<?php endif; ?>
 				<?php
 				++$i;
