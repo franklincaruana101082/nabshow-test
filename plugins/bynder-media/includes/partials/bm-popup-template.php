b3798e9f456cd0b8f2f6dc8a784314862bc02d07
Faisal Alvi <faisalalvi33@gmail.com>
2021-03-11 21:10:39 +0530

    Bynder product image options for backend only

--- plugins/bynder-media/includes/partials/bm-popup-template.php
+++ plugins/bynder-media/includes/partials/bm-popup-template.php
@@ -33,7 +33,7 @@ $requested_by = $this->requested_by;
         }
 
 		// If featured image is requested, skip if featured derivative is absent.
-		if ( 'bm-featured-image' === $requested_by ) {
+		if ( 'profile_picture' === $requested_by ) {
 			foreach ( $derivatives as $derv ) {
 				$pass = 0;
 				if ( 'Featured' === $derv->name ) {
