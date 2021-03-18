6e832a63202b34f0e441ec6c08294119472a24d1
Nitish Kaila <kailanitish90@gmail.com>
2021-03-18 19:49:00 +0530

    exclude sponsor event category from the event calendar view

--- themes/nab-amplify/inc/action/action-functions.php
+++ themes/nab-amplify/inc/action/action-functions.php
@@ -4429,3 +4429,23 @@ function book_sortable_columns( $columns ) {
 	$columns['companyr'] = 'company';
 	return $columns;
 }
+
+/**
+ * Exluded sponsor event category from the event calendar
+ */
+function nab_exclude_events_category( $query ) {
+
+    if ( $query->query_vars['post_type'] === TribeEvents::POSTTYPE && empty( $query->query_vars['suppress_filters'] ) ) {
+        $query->set( 'tax_query', array(
+                array(
+                    'taxonomy' => TribeEvents::TAXONOMY,
+                    'field' => 'slug',
+                    'terms' => array('sponsor-event'),
+                    'operator' => 'NOT IN'
+                )
+            )
+        );
+    }
+
+    return $query;
+}
