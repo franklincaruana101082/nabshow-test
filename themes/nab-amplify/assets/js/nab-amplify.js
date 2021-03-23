ac54a3b360ea7601afbb3bea9d49c2c67178dbfb
Nitish Kaila <kailanitish90@gmail.com>
2021-03-23 20:40:55 +0530

    remove product image upload issue

--- themes/nab-amplify/assets/js/nab-amplify.js
+++ themes/nab-amplify/assets/js/nab-amplify.js
@@ -1066,7 +1066,7 @@
       input.files = dt.files;
     }
   }
-
+  var productMedia = [];
   $(document).on("change", "#product_medias", function (e) {
     var fileExtension = ["png", "jpg", "jpeg", "gif"];
 
@@ -1094,6 +1094,7 @@
             e.target.result
           );
         };
+        productMedia.push($(this));
         var media_count = jQuery(".nab-product-media-item").length;
         if (media_count < 5) {
           reader.readAsDataURL(file);
@@ -1178,8 +1179,8 @@
 
     var form_data = new FormData();
 
-    $.each($("#product_medias")[0].files, function (key, file) {
-      form_data.append(key, file);
+    $.each(productMedia, function (key, file) {
+      form_data.append(key, file[0]);
     });
     if (product_title == "") {
       alert("Product title can not be empty!");
