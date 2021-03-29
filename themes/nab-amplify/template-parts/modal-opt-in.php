bfc514d565f208b825d82422e5e2ead611032eff
Matt Spaanem <spaanem@gmail.com>
2021-03-29 10:07:27 -0700

    fix sessions related bug pushing last related company as opt-in company, clean up modal to only require the user ID and company ID

--- themes/nab-amplify/template-parts/modal-opt-in.php
+++ themes/nab-amplify/template-parts/modal-opt-in.php
@@ -3,6 +3,15 @@ $newTZ = wp_timezone();
 $now = new DateTime(gmdate('Y-m-d H:i:s'));
 $now->setTimezone($newTZ);
 $date_now = $now->format('Y-m-d H:i:s');
+
+$company_name			= get_the_title($company_id);
+$user					= get_user_by( 'id', $user_id );
+$user_first_name 		= get_user_meta( $user_id, "first_name", true);
+$user_last_name 		= get_user_meta( $user_id, "last_name", true);
+$user_email 			= $user->user_email;;
+$user_ip 				= $_SERVER['REMOTE_ADDR'];
+$opt_in_occurred_at_id 	= get_queried_object_id();
+$opt_in_occurred_at_url = get_permalink( get_queried_object_id() );
 ?>
 <div id="modal-opt-in" class="nab-modal">
 	<div class="nab-modal-inner">
@@ -10,7 +19,7 @@ $date_now = $now->format('Y-m-d H:i:s');
 			<span class="nab-modal-close fa fa-times"></span>
 			<div class="modal-content-wrap">
 				<h3>Opt In To Communications</h3>
-				<p><?php echo get_the_title($company);?> would like to communicate with you about this and other products. Please opt-in below.</p>
+				<p><?php echo $company_name;?> would like to communicate with you about this and other products. Please opt-in below.</p>
 				<div class="optin__form">
 					<label class="field__optinButtons">
 						<button type="button" value="true" id="create_opt_in" name="create_opt_in" class="button _gradientpink">Opt In</button>
@@ -30,7 +39,7 @@ jQuery(function($) {
 		type: "POST",
 		data: {
 			action: "nab_check_for_opt_in",
-			'company_id': '<?php echo $company;?>',
+			'company_id': '<?php echo $company_id;?>',
 			'user_id': '<?php echo $user_id;?>'
 		},
 		success: function (data) {
@@ -49,20 +58,20 @@ jQuery(function($) {
 			{
 				"nabNonce": amplifyJS.nabNonce,
 				action: "nab_create_opt_in_out",
-				'post_title': opt.toString() + ' <?php echo($user_firstname . ' ' . $user_lastname . ' : ' . get_the_title($company)); ?>',
+				'post_title': opt.toString() + ' <?php echo($user_first_name . ' ' . $user_last_name . ' : ' . $company_name); ?>',
 				'post_status': 'publish',
 				'post_date': '<?php echo($date_now);?>',
 				'post_author': '<?php echo($user_id);?>',
 				'post_type': 'opt-in',
-				'company_id':'<?php echo($company);?>',
-				'company_name':'<?php echo(get_the_title($company));?>',
+				'company_id':'<?php echo($company_id);?>',
+				'company_name':'<?php echo($company_name);?>',
 				'opted_in':opt,
-				'user_first_name':'<?php echo($user_firstname);?>',
-				'user_last_name':'<?php echo($user_lastname);?>',
+				'user_first_name':'<?php echo($user_first_name);?>',
+				'user_last_name':'<?php echo($user_last_name);?>',
 				'user_email':'<?php echo($user_email);?>',
-				'user_ip':'<?php echo($_SERVER['REMOTE_ADDR']);?>',
-				'opt_in_occurred_at_id':'<?php echo(get_queried_object_id());?>',
-				'opt_in_occurred_at_url':'<?php echo(get_permalink( get_queried_object_id() ));?>'
+				'user_ip':'<?php echo($user_ip);?>',
+				'opt_in_occurred_at_id':'<?php echo($opt_in_occurred_at_id);?>',
+				'opt_in_occurred_at_url':'<?php echo($opt_in_occurred_at_url);?>'
 			},
 			function( data ) {
 				jQuery('#modal-opt-in').hide();	
