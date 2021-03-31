f29b9fe07754517f1e7349f52fd52127818e9c9b
Faisal Alvi <faisalalvi33@gmail.com>
2021-03-31 18:05:16 +0530

    Bynder required fields secured

--- plugins/bynder-media/includes/partials/bm-metas-template.php
+++ plugins/bynder-media/includes/partials/bm-metas-template.php
@@ -10,15 +10,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 $bm_metas = $this->bm_body;
 
 /**
-Media Type = AssetType
-Media Subtype =  AssetSubtype
-Asset Subject = UserType
-Asset Subject Name = UserTypeName [x]
-Community (optional) = Community [already visible]
-Persona (optional) = Persona [already visible]
-Subject / Topic (optional) =  ContentSubject [x]
-Content Scope (optional) = ContentScope [x]
-Year (optional) = Year [x]
+ * Media Type = AssetType
+ * Media Subtype =  AssetSubtype
+ * Asset Subject = UserType
+ * Asset Subject Name = UserTypeName [x]
+ * Event/Product/Content Type = EventType
+ * Community (optional) = Community [already visible]
+ * Persona (optional) = Persona [already visible]
+ * Subject / Topic (optional) =  ContentSubject [x]
+ * Content Scope (optional) = ContentScope [x]
+ * Year (optional) = Year [x]
  */
 
 $keep_only = array(
@@ -33,9 +34,9 @@ $keep_only = array(
 
 $show_when = array(
 	'AssetSubtype' => 'AssetType',
-	'UserTypeName' => 'UserType',
-	/*'ContentSubjectType' => 'ContentSubject',*/
 );
+
+$required_label = array( 'Media Type', 'Media Subtype' );
 
 foreach ( $bm_metas as $meta_name => $mval ) {
 
@@ -44,12 +45,13 @@ foreach ( $bm_metas as $meta_name => $mval ) {
 	}
 
 	$type            = $mval['type'];
+	$mlabel          = in_array( $mval['label'], $required_label, true ) ? $mval['label'] . '*' : $mval['label'];
 	$is_multi_select = $mval['isMultiselect'];
 
 	if ( 0 !== count( $mval['options'] ) ) {
 		?>
         <div class="single-meta" <?php if ( array_key_exists( $meta_name, $show_when ) ) { ?> data-show-when="<?php echo esc_attr( $show_when[ $meta_name ] ); ?>" <?php } ?>>
-            <label class="bm-meta-label"><?php echo esc_html( $mval['label'] ); ?></label>
+            <label class="bm-meta-label"><?php echo esc_html( $mlabel ); ?></label>
             <div class="bm-meta-value" data-name="<?php echo esc_attr( $mval['name'] ); ?>">
 				<?php if ( 'select' === $type && 0 === $is_multi_select ) { ?>
 
