14dde29bb13e41920d88f37f3851b27ddbd0dba9
sadikmultani <hello.sadikmultani@gmail.com>
2021-04-02 17:08:53 +0530

    fixed the company about admin select2 issue

--- themes/nab-amplify/assets/js/nab-amplify.js
+++ themes/nab-amplify/assets/js/nab-amplify.js
@@ -1974,6 +1974,34 @@
             placeholder: "Select Point of contact",
             allowClear: true,
           });
+          $('.company-admins').select2({
+            ajax: {
+              url: amplifyJS.ajaxurl, // AJAX URL is predefined in WordPress admin
+              dataType: 'json',
+              delay: 250, // delay in ms while typing when to perform a AJAX search
+              data: function (params) {
+                return {
+                  q: params.term, // search query
+                  action: 'nab_product_point_of_contact' // AJAX action for admin-ajax.php
+                }
+              },
+              processResults: function (data) {
+                var options = []
+                if (data) {
+                  // data is the array of arrays, and each of them contains ID and the Label of the option
+                  $.each(data, function (index, text) {
+                    // do not forget that "index" is just auto incremented value
+                    options.push({ id: text[0], text: text[1] })
+                  })
+                }
+                return {
+                  results: options
+                }
+              },
+              cache: true
+            },
+            minimumInputLength: 3
+          })
           load_tinyMCE_withPlugins(
             '#company_about',
             '#character-count-comp-about'
