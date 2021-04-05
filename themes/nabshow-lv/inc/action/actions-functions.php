22d40c6314d2bc10719a6b59791b3dc79b822530
sadikmultani <hello.sadikmultani@gmail.com>
2021-04-05 18:41:07 +0530

    updated the block build version

--- themes/nabshow-lv/inc/action/actions-functions.php
+++ themes/nabshow-lv/inc/action/actions-functions.php
@@ -14,13 +14,13 @@ function nabshow_lv_add_block_editor_assets() {
 	wp_register_script( 'nab-gutenberg-block',
 		get_template_directory_uri() . '/blocks/js/block.build.js',
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components', 'wp-dom-ready' ),
-		'6.8'
+		'6.9'
 	);
 
 	wp_enqueue_script( 'nab-custom-gutenberg-block',
 		get_template_directory_uri() . '/blocks/js/nabshow-block.build.js',
 		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'wp-components' ),
-		'6.7'
+		'6.9'
 	);
 
 	wp_register_style(
