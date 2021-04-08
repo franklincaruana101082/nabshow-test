2763993758557154b8f9d9a75c3e9eeb95e1ebb3
Matt Spaanem <spaanem@gmail.com>
2021-04-08 11:12:37 -0700

    change opt-in modal functionality to track in a cookie instead of doing ajax requests to the DB which get cached and break things

--- themes/nab-amplify/single-sessions.php
+++ themes/nab-amplify/single-sessions.php
@@ -423,6 +423,36 @@ if (isset($_GET['registered']) && $_GET['registered'] == 'true') {
 							</div> 
 						</div>
 					</div>
+					<div class="session__desc">
+						<div class="container">
+							<div class="contentwrap">
+							<?php
+							the_content(
+								sprintf(
+									wp_kses(
+										/* translators: %s: Name of current post. Only visible to screen readers */
+										__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'nab-amplify' ),
+										array(
+											'span' => array(
+												'class' => array(),
+											),
+										)
+									),
+									wp_kses_post( get_the_title() )
+								)
+							);
+
+							wp_link_pages(
+								array(
+									'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nab-amplify' ),
+									'after'  => '</div>',
+								)
+							);
+							
+							?>
+							</div>
+						</div>
+					</div>
 					<?php } ?>
 
 				</div><!-- .entry-content -->
