4d003bbbc3b6cdf04dde08269bdcfaed1a861558
Nitish Kaila <kailanitish90@gmail.com>
2021-05-05 16:54:17 +0530

    edit account email changes and keep form fillup when any error

--- themes/nab-amplify/woocommerce/myaccount/form-edit-account.php
+++ themes/nab-amplify/woocommerce/myaccount/form-edit-account.php
@@ -17,6 +17,21 @@
 
 defined( 'ABSPATH' ) || exit;
 
+$account_nonce  = filter_input( INPUT_POST, 'save-account-details-nonce', FILTER_SANITIZE_STRING );
+$account_action = filter_input( INPUT_POST, 'action', FILTER_SANITIZE_STRING );
+
+if ( ( isset( $account_action ) && 'save_account_details' === $account_action ) && ( isset( $account_nonce ) && ! empty( $account_nonce ) ) ) {
+	$first_name 	= filter_input( INPUT_POST, 'account_first_name', FILTER_SANITIZE_STRING );
+	$last_name		= filter_input( INPUT_POST, 'account_last_name', FILTER_SANITIZE_STRING );
+	$display_name	= filter_input( INPUT_POST, 'account_display_name', FILTER_SANITIZE_STRING );
+	$user_email		= filter_input( INPUT_POST, 'account_email', FILTER_SANITIZE_STRING );
+} else {
+	$first_name 	= $user->first_name;
+	$last_name		= $user->last_name;
+	$display_name	= $user->display_name;
+	$user_email		= $user->user_email;
+}
+
 do_action( 'woocommerce_before_edit_account_form' ); ?>
 
 <form class="woocommerce-EditAccountForm edit-account" action="" method="post" <?php do_action( 'woocommerce_edit_account_form_tag' ); ?> >
@@ -25,23 +40,23 @@ do_action( 'woocommerce_before_edit_account_form' ); ?>
 
 	<p class="woocommerce-form-row woocommerce-form-row--first form-row form-row-first">
 		<label for="account_first_name"><?php esc_html_e( 'First name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="<?php echo esc_attr( $user->first_name ); ?>" />
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_first_name" id="account_first_name" autocomplete="given-name" value="<?php echo esc_attr( $first_name ); ?>" />
 	</p>
 	<p class="woocommerce-form-row woocommerce-form-row--last form-row form-row-last">
 		<label for="account_last_name"><?php esc_html_e( 'Last name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="<?php echo esc_attr( $user->last_name ); ?>" />
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_last_name" id="account_last_name" autocomplete="family-name" value="<?php echo esc_attr( $last_name ); ?>" />
 	</p>
 	<div class="clear"></div>
 
 	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 		<label for="account_display_name"><?php esc_html_e( 'Display name', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr( $user->display_name ); ?>" /> <span><em><?php esc_html_e( 'This will be how your name will be displayed throughout the site where you are listed as a commenter, point of contact, author, etc. NOTE: At present, your username is not editable, only your display name.', 'woocommerce' ); ?></em></span>
+		<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="account_display_name" id="account_display_name" value="<?php echo esc_attr( $display_name ); ?>" /> <span><em><?php esc_html_e( 'This will be how your name will be displayed throughout the site where you are listed as a commenter, point of contact, author, etc. NOTE: At present, your username is not editable, only your display name.', 'woocommerce' ); ?></em></span>
 	</p>
 	<div class="clear"></div>
 
 	<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
 		<label for="account_email"><?php esc_html_e( 'Email address', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-		<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="<?php echo esc_attr( $user->user_email ); ?>" />
+		<input type="email" class="woocommerce-Input woocommerce-Input--email input-text" name="account_email" id="account_email" autocomplete="email" value="<?php echo esc_attr( $user_email ); ?>" />
 	</p>
 
 	<fieldset>
