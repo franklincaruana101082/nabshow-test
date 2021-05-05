1b649eda5cc730845798ddb5b03f355c6d7dcb21
PJ Kelly <pj@crushlovely.com>
2021-05-05 16:35:08 -0700

    Segment Consent Manager - Part 1.

--- themes/nab-amplify/header.php
+++ themes/nab-amplify/header.php
@@ -36,6 +36,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div id="consent-manager"></div>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'nab-amplify' ); ?></a>
