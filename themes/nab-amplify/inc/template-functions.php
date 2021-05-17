338dda892fbc5a7463a5bb4ff229f2d39b0c80bf
PJ Kelly <pj@crushlovely.com>
2021-05-17 11:52:55 -0700

    Added source parameters to maritz redirect url.

--- themes/nab-amplify/inc/template-functions.php
+++ themes/nab-amplify/inc/template-functions.php
@@ -1086,7 +1086,7 @@ function nab_maritz_redirect_url( $user_id ) {
 
 	$url = isset( $url_parse['host'] ) && 'amplify.nabshow.com' === $url_parse['host'] ? 'https://registration.experientevent.com/ShowNAB211/Flow/ATT/' : 'https://qawebreg.experientevent.com/ShowNAB211/Flow/ATT/';
 
-	$params		= array( 'user_id' => $user_id );
+	$params		= array( 'user_id' => $user_id, 'registration_flow_id' => wp_generate_uuid4() );
 	$first_name	= get_user_meta( $user_id, 'first_name', true );
 	$last_name	= get_user_meta( $user_id, 'last_name', true );
 	$company	= get_user_meta( $user_id, 'attendee_company', true );
@@ -1111,7 +1111,12 @@ function nab_maritz_redirect_url( $user_id ) {
 	if( isset( $marketing_code ) && ! empty( $marketing_code ) ) {
 		$params['marketing_code'] = $marketing_code;
     }
-
+	if ( is_page( 'sign-up' ) ) {
+		$params['source'] = 'amplify';
+	}
+	if ( is_page( 'nab-show-sign-up' ) ) {
+		$params['source'] = 'nabshow';
+	}
 	return add_query_arg( $params, $url );
 }
 
