146e4cf4ff85bc7843cb1fe7c391dca0ed571ade
Matt Spaanem <spaanem@gmail.com>
2021-08-23 14:50:44 -0700

    not-signed-in partial redirect to page, update article partial display rules

--- themes/nab-amplify/template-parts/not-signed-in.php
+++ themes/nab-amplify/template-parts/not-signed-in.php
@@ -5,15 +5,16 @@
 		
 		<div class="intro__cta">
 			<?php
+			$referring_url = get_permalink( get_queried_object_id() );
 			$sign_up_page = get_page_by_path( NAB_SIGNUP_PAGE ); // @todo later replace this with VIP function
 			if ( isset( $sign_up_page ) && ! empty( $sign_up_page ) ) {
 				$sign_up_page_url = get_permalink( $sign_up_page->ID );
 			?>
-			<a href="<?php echo esc_url( $sign_up_page_url ); ?>" class="button _gradientpink"><?php esc_html_e( 'Sign Me Up', 'nab-amplify' ); ?></a>
+			<a href="<?php echo esc_url( $sign_up_page_url  . '?r=' . $referring_url ); ?>" class="button _gradientpink"><?php esc_html_e( 'Sign Me Up', 'nab-amplify' ); ?></a>
 			<?php } ?>
 
 			
-			<a class="" href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>"><?php esc_html_e( 'Already on NAB Amplify? Sign In', 'nab-amplify' ); ?></a>
+			<a class="" href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' )  . '?r=' . $referring_url ); ?>"><?php esc_html_e( 'Already on NAB Amplify? Sign In', 'nab-amplify' ); ?></a>
 		</div> 
 	</div>
 </div>
