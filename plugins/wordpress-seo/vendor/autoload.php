391bc4cc040285fa78610b3c8294c2d847760011
Nitish Kaila <kailanitish90@gmail.com>
2021-08-23 16:24:00 +0530

    update yoast seo plugin

--- plugins/wordpress-seo/vendor/autoload.php
+++ plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitdad1e30410739153b913ff51e0e2f1cb::getLoader();
+return ComposerAutoloaderInit9569cede6748b69e1da9be4a199a2737::getLoader();
