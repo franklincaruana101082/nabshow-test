327bfc0fdc24f3d06ace44745e1a172815157d3e
Nitish Kaila <kailanitish90@gmail.com>
2021-08-24 19:17:15 +0530

    Made home page static content dynamic using ACF fields

--- themes/nabshow-lv-child-2021/page-home-static2.php
+++ themes/nabshow-lv-child-2021/page-home-static2.php
@@ -77,39 +77,58 @@ if ( have_posts() ) :
   endwhile; // End of the loop.
 endif;
 
-?>
+$quick_links          = get_field( 'quick_links' );
+$testimonials         = get_field( 'testimonials' );
+$attending_companies  = get_field( 'attending_companies' );
+$exhibitor_companies  = get_field( 'exhibitor_companies' );
+$display_safety       = get_field( 'display_safety' );
 
-
+if ( is_array( $quick_links ) && count( $quick_links ) > 0 ) {
+  ?>
   <div class="container _pull-up">
     <div class="jump-links">
       <h2 class="jump-links__label">Quick Links:</h2>
       <ul class="jump-links__menu">
-        <li class="jump-links__item"><a href="<?php echo site_url('/attend/'); ?>" class="button _arrow _full">Attend</a></li>
-        <li class="jump-links__item"><a href="<?php echo site_url('/resources-for/press/'); ?>" class="button _arrow _full">Press</a></li>
-        <li class="jump-links__item"><a href="<?php echo site_url('/exhibit/'); ?>" class="button _arrow _full">Exhibit</a></li>
-        <li class="jump-links__item"><a href="<?php echo site_url('/sponsor/'); ?>" class="button _arrow _full">Sponsor</a></li>
+        <?php
+        foreach ( $quick_links as $link ) {
+          
+          $link_url     = $link['link']['url'];
+          $link_title   = $link['link']['title'];
+          $link_target  = isset( $link['link']['target'] ) && ! empty( $link['link']['target'] ) ? $link['link']['target'] : '_self';
+          ?>
+          <li class="jump-links__item"><a href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>" class="button _arrow _full"><?php echo esc_html( $link_title ); ?></a></li>
+          <?php
+        }
+        ?>
       </ul>
     </div>
   </div>
+  <?php
+}
 
+if ( is_array( $testimonials ) && count( $testimonials ) > 0 ) {
+  ?>
   <div class="testimonials-wrapper">
     <div class="container">
       <div class="testimonials">
-        <figure class="testimonial">
-          <blockquote>The ability to meet with a large number of our current and potential customers is the reason we return to NAB Show year after year, and that opportunity was sorely missed in 2020.</blockquote>
-          <figcaption>Ken Frommert, President, Enco Systems, Inc</figcaption>
-        </figure>
-        <figure class="testimonial">
-          <blockquote>The AES is eagerly anticipating being face-to-face with our peers in-person in Las Vegas! Our colocation with NAB Show is the perfect time to reconnect as our industry regains its footing.</blockquote>
-          <figcaption>Colleen Harper, Executive Director, AES</figcaption>
-        </figure>
-        <figure class="testimonial">
-          <blockquote>I love discovering innovations from startups in Futures Park, finding new technology on the exhibit floor, and networking... I’m  energized just thinking about the chance to see everyone face-to-face again.</blockquote>
-          <figcaption>Brett Jenkins, CTO, Nextar Media Group</figcaption>
-        </figure>
+        <?php
+        foreach ( $testimonials as $testimonial ) {
+          $quote  = $testimonial['quote'];
+          $source = $testimonial['source'];
+          ?>
+          <figure class="testimonial">
+            <blockquote><?php echo esc_html( $quote ); ?></blockquote>
+            <figcaption><?php echo esc_html( $source ); ?></figcaption>
+          </figure>
+          <?php
+        }
+        ?>
       </div>
     </div>
   </div>
+  <?php
+}
+?>
 
 <?php /*
   <div class="section _bottom-only container">
@@ -117,122 +136,146 @@ endif;
   </div>
 */ ?>
 
+<?php
+if ( is_array( $attending_companies ) && count( $attending_companies ) > 0 ) {
+  ?>
   <div class="section _bottom">
     <div class="container">
       <div class="attending">
         <h3 class="attending-subtitle">Thousands of attendees, hundreds of exhibitors are ready</h3>
         <h2 class="h-xl attending-title">You’ll Be In Good Company</h2>
           <div class="attending-panel">
-            <span class="attending-item">
-              <b class="attending-item-name">ABC</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">CBS</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Dallas Cowboys</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Department of Defense</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Discovery Channel</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Disney Streaming Services</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Dreamworks</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Epic Games</b>
-            </span>        
-            <span class="attending-item">
-              <b class="attending-item-name">ESPN</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Facebook</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">FIFA</b>
-            </span>
-            <span class="attending-item">
-              <b class="attending-item-name">Formula 1</b>
-            </span>
+            <?php
+            foreach ( $attending_companies as $company ) {
+              
+              if ( ! empty( $company['company'] ) ) {
+                ?>
+                <span class="attending-item">
+                  <b class="attending-item-name"><?php echo esc_html( $company['company'] ); ?></b>
+                </span>
+                <?php
+              }
+            }
+            ?>
           </div>
-          
           <div class="attending-more">
             <a href="<?php echo site_url('/youll-be-in-good-company/'); ?>" class="button _solid">View more</a>
           </div>
-          
       </div>
     </div>
   </div>
+  <?php
+}
 
+if ( is_array( $exhibitor_companies ) && count( $exhibitor_companies ) > 0 ) {
+  ?>
   <div class="section _bottom exhibiting-section">
     <div class="container exhibiting-container">
       <div class="exhibiting">
         <h3 class="exhibiting-subtitle">Discover new tech and solutions</h3>
         <h2 class="h-xl exhibiting-title">Explore the hundreds of exhibiting companies</h2>
         <div class="logo-group _small exhibiting-logo-group">
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-sony.png" alt="SONY"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-canon.png" alt="canon"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-telestream.png" alt="telestream"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-panasonic.png" alt="panasonic"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-mediakind.png" alt="mediakind"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-vizrt.png" alt="vizrt"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-aws.png" alt="AWS"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-aja.png" alt="AJA"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-grassvalley.png" alt="grass valley"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-chyron.png" alt="chyron"/>
-          <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/exhibitor-ross.png" alt="ross"/>
+          <?php
+          foreach ( $exhibitor_companies as $exhibitor_company ) {
+
+            $logo_name  = $exhibitor_company['name'];
+            $logo       = isset( $exhibitor_company['logo']['ID'] ) && ! empty( $exhibitor_company['logo']['ID'] ) ? wp_get_attachment_url( $exhibitor_company['logo']['ID'] ) : '';
+
+            if ( ! empty( $logo_name ) && ! empty( $logo ) ) {
+              ?>
+              <img src="<?php echo esc_url( $logo ); ?>" alt="<?php echo esc_attr( $logo_name ); ?>"/>
+              <?php
+            }
+          }
+          ?>
         </div>
       </div>
     </div>
   </div>
+  <?php
+}
+?>
 
   <div class="banner">
     <img src="https://nabshow.com/2021/wp-content/uploads/sites/4/2021/07/Homepage-wide-angle.jpg" alt="NAB Show: An unrivaled audio and video experience. Together with: Radio Show, SMTE, AES Show Fall 2021, and BEA." class="banner__image">
   </div>
 
-  <div class="safety">
-    <div class="container section">
-      <h2 class="h-lg safety__title">THE SAFETY OF OUR COMMUNITY IS THE #1 PRIORITY</h2>
-      <div class="safety__icons">
-        <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/icon-sanitizer.png" alt="Sanitizer icon" />
-        <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/icon-distancing.png" alt="Social distancing icon" />
-        <img src="/wp-content/themes/nabshow-lv-child-2021/assets/images/icon-broom.png" alt="Broom icon" />
-      </div>
-      <div class="safety__copy">
-        <p>Creating a healthy and safe environment for visitors to NAB Show remains a top priority. We are following all health and safety protocols as outlined by federal and local governments. As information continues to develop, we will adjust our approach as needed.</p>
-      </div>
+<?php
+  if ( $display_safety ) {
+    
+    $safety_title       = get_field( 'safety_title' );
+    $safety_icons       = get_field( 'safety_icons' );
+    $safety_copy        = get_field( 'safety_copy' );
+    $safety_stats       = get_field( 'safety_stats' );
+    $safety_fine_print  = get_field( 'safety_fine_print' );
 
-      <ul class="safety__stats">
-        <li class="safety__stat">
-          <div class="safety__statTitle">Optimism is on the rise</div>
-          <div class="safety__statNumber">
-            79<span class="safety__statPercent">%</span>
+    ?>
+    <div class="safety">
+      <div class="container section">
+        <?php
+        if ( ! empty( $safety_title ) ) {
+          ?>
+          <h2 class="h-lg safety__title"><?php echo esc_html( $safety_title ); ?></h2>
+          <?php
+        }
+
+        if ( is_array( $safety_icons ) && count( $safety_icons ) > 0 ) {
+          ?>
+          <div class="safety__icons">
+            <?php
+            foreach ( $safety_icons as $icon ) {
+              
+              $alt  = isset( $icon['icon']['alt'] ) && ! empty( $icon['icon']['alt'] ) ? $icon['icon']['alt'] : 'Safety Icon';
+              $logo = isset( $icon['icon']['ID'] ) && ! empty( $icon['icon']['ID'] ) ? wp_get_attachment_url( $icon['icon']['ID'] ) : '';
+              ?>
+              <img src="<?php echo esc_url( $logo ); ?>" alt="<?php echo esc_attr( $alt ); ?>" />
+              <?php
+            }
+            ?>
           </div>
-          <div class="safety__statFollow">are highly likely to attend a live experience this year</div>
-        </li>
-        <li class="safety__stat">
-          <div class="safety__statTitle">There’s nothing like face to face</div>
-          <div class="safety__statNumber">
-            87<span class="safety__statPercent">%</span>
+          <?php
+        }
+
+        if ( ! empty( $safety_copy ) ) {
+          ?>
+          <div class="safety__copy">
+            <p><?php echo esc_html( $safety_copy ); ?></p>
           </div>
-          <div class="safety__statFollow">find in-person events "irreplaceable"</div>
-        </li>
-        <li class="safety__stat">
-          <div class="safety__statTitle">Excited about what's new</div>
-          <div class="safety__statNumber">
-            62 <span class="safety__statPercent">%</span>
-          </div>
-          <div class="safety__statFollow">prefer evaluating products at events</div>
-        </li>
-      </ul>
-      <div class="safety__fineprint">*Based on NAB Show attendee survey data</div>
-    </div>  
-  </div>
+          <?php
+        }
+
+        if ( is_array( $safety_stats ) && count( $safety_stats ) > 0 ) {
+          ?>
+          <ul class="safety__stats">
+            <?php
+            foreach ( $safety_stats as $stat ) {
+              
+              ?>
+              <li class="safety__stat">
+                <div class="safety__statTitle"><?php echo esc_html( $stat['title'] ); ?></div>
+                <div class="safety__statNumber">
+                  <?php echo esc_html( $stat['number'] ); ?><span class="safety__statPercent"><?php echo esc_html( $stat['unit'] ); ?></span>
+                </div>
+                <div class="safety__statFollow"><?php echo esc_html( $stat['subtext'] ); ?></div>
+              </li>
+              <?php
+            } 
+            ?>
+          </ul>
+          <?php
+        }
+
+        if ( ! empty( $safety_fine_print ) ) {
+          ?>
+          <div class="safety__fineprint"><?php echo esc_html( $safety_fine_print ); ?></div>
+          <?php
+        }
+        ?>
+      </div>  
+    </div>
+    <?php
+  }
+?>
 
 
 <div class="container">
