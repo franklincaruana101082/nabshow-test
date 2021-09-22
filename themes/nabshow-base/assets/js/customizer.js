14660ff46dbefbd370d164db4ff940d91f298eab
Matt Spaanem <spaanem@gmail.com>
2021-09-22 07:46:28 -0700

    customizer color options functionality

--- themes/nabshow-base/assets/js/customizer.js
+++ themes/nabshow-base/assets/js/customizer.js
@@ -7,7 +7,7 @@
  */
 
 ( function( $ ) {
-
+	$('body').addClass('ready');
 	// Site title and description.
 	wp.customize( 'blogname', function( value ) {
 		value.bind( function( to ) {
@@ -21,22 +21,116 @@
 	} );
 
 	// Header text color.
-	wp.customize( 'header_textcolor', function( value ) {
+	wp.customize( 'primary_color', function( value ) {
+		console.log('primary color changed');
 		value.bind( function( to ) {
 			if ( 'blank' === to ) {
-				$( '.site-title, .site-description' ).css( {
-					'clip': 'rect(1px, 1px, 1px, 1px)',
-					'position': 'absolute'
+				$( ':root' ).css( {
+					'--primary-color': '#2a173c',
+					'--primary-slate': '#817a89',
+					'--primary-slate-dark': '#2d2930',
+					'--primary-lighter': '#cd8ec4',
+					'--primary-light': '#9e4593',
+					'--primary-lightish': '#6b3b99',
+					'--primary-dark': '#030621',
 				} );
 			} else {
-				$( '.site-title, .site-description' ).css( {
-					'clip': 'auto',
-					'position': 'relative'
+				var primaryHSV       = hex2hsv(to);
+				var primarySlate     = hsv2rgb(primaryHSV[0]-2.8,primaryHSV[1]*0.176661264,primaryHSV[2]*2.2851);
+				var primarySlateDark = hsv2rgb(primaryHSV[0]+3.5,primaryHSV[1]*0.236628849,primaryHSV[2]*0.8);
+				var primaryLighter   = hsv2rgb(primaryHSV[0]+37.8,primaryHSV[1]*0.497568882,primaryHSV[2]*3.421276596);
+				var primaryLight     = hsv2rgb(primaryHSV[0]+36.6,primaryHSV[1]*0.912479741,primaryHSV[2]*2.638297872);
+				var primaryLightish  = hsv2rgb(primaryHSV[0]-0.2,primaryHSV[1]*0.995137763,primaryHSV[2]*2.553191489);
+				var primaryDark      = hsv2rgb(primaryHSV[0]-36.8,primaryHSV[1]*1.473257699,primaryHSV[2]*0.54893617);
+				$( ':root' ).css( {
+					'--primary-color': to,
+					'--primary-slate': primarySlate,
+					'--primary-slate-dark': primarySlateDark,
+					'--primary-lighter': primaryLighter,
+					'--primary-light': primaryLight,
+					'--primary-lightish': primaryLightish,
+					'--primary-dark': primaryDark,
 				} );
-				$( '.site-title a, .site-description' ).css( {
-					'color': to
+				
+			}
+		} );
+	} );
+	wp.customize( 'secondary_color', function( value ) {
+		value.bind( function( to ) {
+			if ( 'blank' === to ) {
+				$( ':root' ).css( {
+					'--secondary-color': '#2f5dab'
 				} );
+			} else {
+				$( ':root' ).css( {
+					'--secondary-color': to
+				} );
+				
+			}
+		} );
+	} );
+	wp.customize( 'tertiary_color', function( value ) {
+		value.bind( function( to ) {
+			if ( 'blank' === to ) {
+				$( ':root' ).css( {
+					'--tertiary-color': '#f4b2ec'
+				} );
+			} else {
+				$( ':root' ).css( {
+					'--tertiary-color': to
+				} );
+				
 			}
 		} );
 	} );
 } )( jQuery );
+
+
+function hex2hsv(hex) {
+	var r = parseInt(hex.substr(1,2), 16); // Grab the hex representation of red (chars 1-2) and convert to decimal (base 10).
+	var g = parseInt(hex.substr(3,2), 16);
+	var b = parseInt(hex.substr(5,2), 16);
+
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+	var h, s, v = max;
+
+	var d = max - min;
+	s = max == 0 ? 0 : d / max;
+
+	if (max == min) {
+	h = 0; // achromatic
+	} else {
+	switch (max) {
+	  case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+	  case g: h = (b - r) / d + 2; break;
+	  case b: h = (r - g) / d + 4; break;
+	}
+
+	h /= 6;
+	}
+
+	return [ h, s, v ];
+    
+}
+
+function hsv2rgb(h, s, v) {
+  var r, g, b;
+
+  var i = Math.floor(h * 6);
+  var f = h * 6 - i;
+  var p = v * (1 - s);
+  var q = v * (1 - f * s);
+  var t = v * (1 - (1 - f) * s);
+
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+
+  return 'rgb('+r * 255+', '+g * 255+', '+b * 255+')';
+}
