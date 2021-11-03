48bc836187d68ffec33341cdbfe4607d97b12d0a
Valerie DuBreuil <val@chiselandbrand.com>
2021-11-03 09:08:17 -0400

    Updating confirmation for BEIT
    per JIRA ticket https://nabshow.atlassian.net/jira/software/projects/NDTI/boards/9?selectedIssue=NDTI-791&text=confirm.
    
    NOTE: Also hard added to prod.

--- themes/nab-amplify/woocommerce/emails/nab-registration-receipt.php
+++ themes/nab-amplify/woocommerce/emails/nab-registration-receipt.php
@@ -5,10 +5,9 @@ do_action( 'woocommerce_email_header', $email_heading, $email );
 
 ?>
 	<p><?php printf( esc_html__( 'Dear %1s %2s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ), esc_html( $order->get_billing_last_name() ) ); ?></p>
-	<p>You’re registered!</p>
-	<p>We look forward to hosting you and other professionals from the media and entertainment industry this October.</p>
-	<p>To view and/or add items to your registration or make changes to your NAB Amplify account, simply <a
-			href="<?php echo esc_url( wc_get_account_endpoint_url( 'edit-account' ) ); ?>">Log In</a>.</p>
+	<p>You’re all set!</p>
+	<p>Access to the online 75th annual Broadcast Engineering & IT (BEIT) Conference VOD presentations is now available <a href="https://amplify.nabshow.com/beit-online-2021/">here</a>. A link to the BEIT Conference Proceedings pdf will be emailed to you shortly.</p>
+
 
 <?php
 
@@ -36,15 +35,11 @@ if( 'yes' !== $is_bulk_child ) {
 }
 ?>
 
-	<p>Details on accessing the event will hit your inbox in the coming weeks. Have questions in the meantime? Contact us at <a
-			href="mailto:register@nab.org">register@nab.org</a>.</p>
+<p>To view and/or add items to your registration or make changes to your NAB Amplify account, simply <a
+		href="<?php echo esc_url( wc_get_account_endpoint_url( 'edit-account' ) ); ?>">Log In</a>. Have questions? Contact us at <a href="mailto:support@nabamplify.zendesk.com">support@nabamplify.zendesk.com</a>.</p>
+
 	<p>Stay Amped!<br>
 		Team NAB</p>
-	<p>Radio Show<br>October 5 - 9, 2020<br><a href="https://www.radioshowweb.com/">radioshowweb.com</a></p>
-
-	<p>Sales and Management Television Exchange<br>October 14 - 15, 2020<br><a href="http://nabsmte.com/">nabsmte.com</a></p>
-
-	<p>NAB Show New York<br>October 19 - 29, 2020<br><a href="https://nabshowny.com/">nabshowny.com</a></p>
 
 <?php
 /**
