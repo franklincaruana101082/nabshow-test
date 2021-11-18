2b937c2b449d58ab8b3e295558f9f556dfa8e6ab
Matt Spaanem <spaanem@gmail.com>
2021-11-18 15:39:58 -0800

    resource templates updates

--- themes/nabshow-base/functions.php
+++ themes/nabshow-base/functions.php
@@ -750,3 +750,21 @@ function nabshow_lv_2021_session_filter() {
 }
 add_action( 'wp_ajax_nab_2021_session_filter', 'nabshow_lv_2021_session_filter' );
 add_action( 'wp_ajax_nopriv_nab_2021_session_filter', 'nabshow_lv_2021_session_filter' );
+
+
+function fix_post_id_on_preview($null, $post_id) {
+    if (is_preview()) {
+        return get_the_ID();
+    }
+    else {
+        $acf_post_id = isset($post_id->ID) ? $post_id->ID : $post_id;
+
+        if (!empty($acf_post_id)) {
+            return $acf_post_id;
+        }
+        else {
+            return $null;
+        }
+    }
+}
+add_filter( 'acf/pre_load_post_id', 'fix_post_id_on_preview', 10, 2 );
