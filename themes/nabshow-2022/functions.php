2b7f779b155b322c5eb6b3b08d5aa80f0011e98e
Matt Spaanem <spaanem@gmail.com>
2021-12-14 14:13:56 -0800

    add swiftype script in a way that works

--- themes/nabshow-2022/functions.php
+++ themes/nabshow-2022/functions.php
@@ -15,4 +15,18 @@ function nabshow_2022_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'nabshow_2022_enqueue_styles', 101 );
 
+function nabshow_2022_swiftype_script() {
+    $swiftype_key = 'xR-5CQfLTC5vmf-SyuiJ';
+    ?>
+    <script type="text/javascript">
+      (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+      (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+      e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+      })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+      
+      _st('install','<?php echo($swiftype_key); ?>','2.0.0');
+    </script>
+    <?php
+}
 
+add_action('wp_footer', 'nabshow_2022_swiftype_script');
