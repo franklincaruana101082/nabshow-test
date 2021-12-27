5015e854936f0889bfebe8b03bc0644b81d499e3
Nitish Kaila <kailanitish90@gmail.com>
2021-12-27 14:04:49 +0530

    removed plugin

--- plugins/automatewoo/admin/assets/src/base/components/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Export all components
- */
-export { default as ProgressBar } from './progress-bar';
-export { default as Button } from './button';
-export { default as WorkflowSearch } from './workflow-search';
