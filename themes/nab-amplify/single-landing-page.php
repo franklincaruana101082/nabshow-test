6d5a812c85cc69d7604e91bfdf1a858a9f0e2892
Matt Spaanem <spaanem@gmail.com>
2022-04-13 08:45:52 -0700

    QR landing page header updates

---
+++ themes/nab-amplify/single-landing-page.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying landing pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Amplify
+ */
+
+if (get_field('remove_search_from_page_header')) {
+	get_header('nosearch');	
+} else {
+	get_header();
+}
+$custom_class = get_field('custom_class');
+?>
+
+	<!-- START legacy-template: single -->
+	<div class="container">
+	<main id="primary" class="site-main single_php <?php echo esc_attr($custom_class); ?>">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );			
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+	</div><!-- .container -->
+	<!-- END legacy-template -->
+
+<?php
+get_footer();
