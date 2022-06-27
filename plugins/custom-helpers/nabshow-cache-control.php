af3b21750bdc6830da90f1bf4633a10fa0b11cbf
franklin caruana <franklin@solveware.co>
2022-06-27 23:55:34 +0800

    chore: clearing loop

--- plugins/custom-helpers/nabshow-cache-control.php
+++ plugins/custom-helpers/nabshow-cache-control.php
@@ -60,19 +60,10 @@ class NabshowCacheControl extends Vary_Cache{
 		// phpcs:ignore WordPress.Security.NonceVerification.Missing
 		$is_user_in_nabshow = self::is_user_in_group_segment( 'nabshow', 'yes' );
 		if( !empty($is_user_in_nabshow) ){
-			if(is_user_logged_in()) self::enable_encryption();
+			// if(is_user_logged_in()) self::enable_encryption();
 			
 		}else{
 			self::set_group_for_user( 'nabshow', 'yes' );
-		}
-
-		if( !empty($is_user_in_nabshow) ){
-			// Redirect back to the same page (per the POST-REDIRECT-GET pattern).
-			// Please note the use of the `vip_vary_cache_did_send_headers` action.
-			add_action( 'vip_vary_cache_did_send_headers', function() {
-				wp_safe_redirect( add_query_arg( '' ) );
-				exit;
-			} );
 		}
 	}
 }
