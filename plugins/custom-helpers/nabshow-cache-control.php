3c71a5fa2a26e1eab028332541201ad9376b41a2
franklin caruana <franklin@solveware.co>
2022-07-01 04:37:19 +0800

    enhance: removing session scripts

--- plugins/custom-helpers/nabshow-cache-control.php
+++ plugins/custom-helpers/nabshow-cache-control.php
@@ -35,56 +35,9 @@ class NabshowCacheControl
     public function init_enqueue_scripts()
     {        
         add_filter('wp_headers', [ $this, 'set_nabshow_frame_options_header'], 999);
-        // add_action( 'wp_enqueue_scripts', array( $this, 'set_clearing_session_js'));
 
     }//end init_enqueue_scripts()
 
-
-    public function set_clearing_session_js( ){
-        global $post;
-            
-            $current_page   = '';
-            $search_terms   = '';
-            $content_type   = '';
-            $is_pageview    = true;
-
-            if ( is_search() ) {            
-                $current_page   = 'search';
-                $search_terms   = get_search_query();
-                $content_type   = filter_input( INPUT_GET, 'v', FILTER_SANITIZE_STRING );
-                $content_type   = isset( $content_type ) ? $content_type : '';
-            }
-            
-            if ( is_admin() || ! isset( $post->ID ) || empty( $post->ID ) || is_post_type_archive() || is_tax() || is_search() ) {
-                $is_pageview = false;
-            }
-
-            if ( $is_pageview ) {
-                                                
-                if ( isset( $post->ID ) && ! empty( $post->ID ) ) {
-                    
-                    if ( is_front_page() ) {
-                        $current_page = 'home';
-                    } else {
-                        $current_page = 'other';
-                    }                
-                }
-            }
-
-        wp_enqueue_script( 'invalidate-sessions-js', plugin_dir_url( __DIR__ ) . 'custom-helpers/url-env-cache-control-helper/invalidate-sessions.js' );
-        wp_localize_script( 'invalidate-sessions-js', 'invalidate_sessions', array(
-            'ajaxurl'       => admin_url('admin-ajax.php'),
-            'nabNonce'      => wp_create_nonce('nab-ajax-nonce'),
-            'postID'        => (!empty($post->ID) ? $post->ID : 0),
-            'page'          => $current_page,
-            'search_term'   => $search_terms,
-            'is_pageview'   => $is_pageview,
-            'content_type'  => $content_type,
-        ));
-
-
-        UrlCacheControl::invalidate_sessions();
-    }
 
     public function set_nabshow_frame_options_header( $headers )
     {
