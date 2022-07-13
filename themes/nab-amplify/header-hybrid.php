a011462b845d0da083263a57349a7627f6004383
PJ Kelly <pj@crushlovely.com>
2022-07-13 11:51:09 -0700

    Alternate ConsentManager config.

--- themes/nab-amplify/header-hybrid.php
+++ themes/nab-amplify/header-hybrid.php
@@ -73,6 +73,7 @@ switch_to_blog('4');
 </head>
 
 <body <?php body_class(); ?>>
+<div id="target-container"></div>
 <?php wp_body_open(); ?>
 <div id="page" class="site nabshow_2021 nab_hybrid">
 
--- themes/nab-amplify/template-parts/tracking-segment.php
+++ themes/nab-amplify/template-parts/tracking-segment.php
@@ -1,5 +1,5 @@
 <?php
-    // $segment_write_key    = vip_get_env_var( 'SEGMENT_AMPLIFY_WRITE_KEY' );
+    $segment_write_key    = vip_get_env_var( 'SEGMENT_AMPLIFY_WRITE_KEY' );
     $post_id = get_the_id();
     $post_type = get_post_type( $post_id );
     $app_name = "amplify";
@@ -82,3 +82,58 @@
     analytics.page({ content_id: "<?php echo $post_id; ?>", content_type: "<?php echo $post_type; ?>" });
     }}();
   </script>
+
+<script>
+  window.consentManagerConfig = function(exports) {
+    var writeKey = '<?php echo $segment_write_key; ?>';
+    var React = exports.React
+    var inEU = exports.inEU
+
+    var bannerContent = React.createElement(
+      'span',
+      null,
+      'We use cookies (and other similar technologies) to collect data to improve your experience on our site. By using our website, you՚re agreeing to the collection of data as described in our',
+      ' ',
+      React.createElement(
+        'a',
+        { href: '/privacy-policy/', target: '_blank' },
+        'Website Data Collection Policy'
+      ),
+      '.'
+    )
+    var bannerSubContent = 'You can change your preferences at any time.'
+    var preferencesDialogTitle = 'Website Data Collection Preferences'
+    var preferencesDialogContent =
+      'We use data collected by cookies and JavaScript libraries to improve your browsing experience, analyze site traffic, deliver personalized advertisements, and increase the overall performance of our site.'
+    var cancelDialogTitle = 'Are you sure you want to cancel?'
+    var cancelDialogContent =
+      'Your preferences have not been saved. By continuing to use our website, you՚re agreeing to our Website Data Collection Policy.'
+
+    return {
+      container: '#target-container',
+      writeKey: writeKey,   
+      shouldRequireConsent: inEU,
+      bannerContent: bannerContent,
+      bannerSubContent: bannerSubContent,
+      preferencesDialogTitle: preferencesDialogTitle,
+      preferencesDialogContent: preferencesDialogContent,
+      cancelDialogTitle: cancelDialogTitle,
+      cancelDialogContent: cancelDialogContent
+    }
+  }
+</script>
+
+<script
+  src="https://unpkg.com/@segment/consent-manager@5.6.0/standalone/consent-manager.js"
+  defer
+></script>
+
+<style>
+    #target-container {
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 9999999;
+    }
+</style>
