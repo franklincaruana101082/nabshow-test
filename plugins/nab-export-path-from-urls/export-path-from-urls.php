5a1d7bf1c34420a5e5c5ac7981f1caedc1e1bec3
franklin caruana <franklin@solveware.co>
2022-07-15 21:22:13 +0800

    fix: clearing phpcs issues

--- plugins/nab-export-path-from-urls/export-path-from-urls.php
+++ plugins/nab-export-path-from-urls/export-path-from-urls.php
@@ -17,19 +17,6 @@ function eau_include_settings_page(){
 
 }
 
-function eau_export_all_urls_on_activate() {
-    if ( version_compare( PHP_VERSION, '5.4', '<' ) ) {
-        deactivate_plugins( plugin_basename( __FILE__ ) );
-        $plugin_data = get_plugin_data( __FILE__ );
-        $plugin_version = $plugin_data['Version'];
-        $plugin_name = $plugin_data['Name'];
-        wp_die( '<h1>' . __('Could not activate plugin: PHP version error') . '</h1><h2>PLUGIN: <i>' . $plugin_name . ' ' . $plugin_version . '</i></h2><p><strong>' . __('You are using PHP version') . ' ' . PHP_VERSION . '</strong>. ' . __( 'This plugin has been tested with PHP versions 5.4 and greater.') . '</p><p>' . __('WordPress itself recommends using PHP version 7.3 or greater') . ': <a href="https://wordpress.org/about/requirements/" target="_blank">' . __('Official WordPress requirements') . '</a>' . '. ' . __('Please upgrade your PHP version or contact your Server administrator.') . '</p>', __('Could not activate plugin: PHP version error'), array( 'back_link' => true ) );
-
-    }
-    set_transient( 'eau_export_all_urls_activation_redirect', true, 30 );
-}
-
-register_activation_hook( __FILE__, 'eau_export_all_urls_on_activate' );
 
 function eau_redirect_on_export_all_urls_activation() {
 
