f16fa9bc6367fe079491b0b89eede4145ff0f82d
franklin caruana <franklin@solveware.co>
2022-07-18 06:17:05 +0800

    fix: script update clearing scripts

--- plugins/nab-export-path-from-urls/nab-export-path-from-urls.php
+++ plugins/nab-export-path-from-urls/nab-export-path-from-urls.php
@@ -20,55 +20,67 @@ if (! defined('ABSPATH')) {
     exit;
     // Exit if accessed directly.
 }
-
 require_once (plugin_dir_path(__FILE__) . 'functions.php');
-require_once( WP_PLUGIN_DIR . '/nab-export-path-from-urls/export-path-from-urls.php' );
+require_once( plugin_dir_path(__FILE__) . 'export-path-from-urls.php' );
 
+add_action( 'plugins_loaded', 'my_plugin_override' );
 
-add_filter( 'plugin_action_links', 'disable_export_paths_plugin_deactivation', 10, 2  );
+function my_plugin_override() {
+	add_filter( 'plugin_action_links', 'disable_export_paths_plugin_deactivation', 10, 2  );
 
-// Hide Activate/Deactivate Plugin Link
-function disable_export_paths_plugin_deactivation( $actions, $plugin_file ) {
- 
-    if ( plugin_basename( __FILE__ ) === $plugin_file ) {
-		unset( $actions['deactivate'], $actions['activate'] );
+	// Hide Activate/Deactivate Plugin Link
+	function disable_export_paths_plugin_deactivation( $actions, $plugin_file ) {
+
+		if ( plugin_basename( __FILE__ ) === $plugin_file ) {
+			unset( $actions['deactivate'], $actions['activate'] );
+		}
+
+		return $actions;
+	}
+	add_action( 'after_setup_theme', 'setup_custom_plugins' );
+
+	function setup_custom_plugins() {
+
+		add_theme_support( 'title-tag' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'custom-header' );
+
 	}
 
-	return $actions;
+	// This function attached on the_content hook is intended to display the CSV Content.
+	// Can be In Admin or anywhere from site.. That includes Nabshow Amplify
+	function nabshow_content_after_body($content){
+		$rowdata = retrieve_exported_file_to_array();
+
+		if(empty($rowdata)) return $content;
+
+		$rowspagination = get_rows_pagination($rowdata);
+
+		$content = $rowspagination;
+
+
+		$escjs =  "<script>
+					jQuery(function(){
+						var body = document.body,
+						html = document.documentElement;
+
+						var height = Math.max( body.scrollHeight, body.offsetHeight,
+										html.clientHeight, html.scrollHeight, html.offsetHeight );
+						var scroll_pos=(height);
+						jQuery('html, body').animate({scrollTop:(scroll_pos)}, '2000');
+					});
+				</script>";
+
+		$content .= $escjs;
+
+		return $content;
+	}
+
+	add_filter('the_content','nabshow_content_after_body');
+
+	if(is_multisite()){
+		add_filter('upload_dir', 'fix_upload_paths');
+	}
+
 }
 
-
-// This function attached on the_content hook is intended to display the CSV Content.
-// Can be In Admin or anywhere from site.. That includes Nabshow Amplify
-function nabshow_content_after_body($content){
-	$rowdata = retrieve_exported_file_to_array();
-	
-	if(empty($rowdata)) return $content;	
-
-	$rowspagination = get_rows_pagination($rowdata);
-
-	$content = $rowspagination;
-
-
-	$escjs =  "<script>
-				jQuery(function(){
-					var body = document.body,
-					html = document.documentElement;
-
-					var height = Math.max( body.scrollHeight, body.offsetHeight,
-									html.clientHeight, html.scrollHeight, html.offsetHeight );
-					var scroll_pos=(height);
-					jQuery('html, body').animate({scrollTop:(scroll_pos)}, '2000');
-				});
-			</script>";
-
-	$content .= $escjs;
-
-	return $content;
-}
-
-add_filter('the_content','nabshow_content_after_body');
-
-if(is_multisite()){
-    add_filter('upload_dir', 'fix_upload_paths');
-}
