c8ddbc4416343f17c8b5bc8eb8e21fe30f7a2d1b
franklin caruana <franklin@solveware.co>
2022-07-30 02:03:19 +0800

    fix: undefined folder name

---
+++ nab-export-path-from-urls/cls-export-all-path-from-urls-settings.php
@@ -0,0 +1,532 @@
+<?php
+
+namespace Plugins\NabExportPathFromUrls;
+
+// require_once (plugin_dir_path(__FILE__) . 'export-all-paths.php');
+
+class ExportAllPathFromUrlsSettings extends \Plugins\NabExportPathFromUrls\ExportAllPaths
+{
+	public function __construct()
+	{
+		if (!current_user_can('manage_options'))
+		{
+			wp_die('You do not have sufficient permissions to access this page.');
+		}
+
+		$custom_posts_names = array();
+		$custom_posts_labels = array();
+		$user_ids = array();
+		$user_names = array();
+
+		$args = array(
+			'public' => true,
+			'_builtin' => false
+		);
+
+		$output = 'objects';
+
+		$operator = 'and';
+
+		$post_types = get_post_types($args, $output, $operator);
+
+		foreach ($post_types as $post_type)
+		{
+
+			$custom_posts_names[] = $post_type->name;
+			$custom_posts_labels[] = $post_type
+				->labels->singular_name;
+
+		}
+
+		$users = get_users();
+
+		foreach ($users as $user)
+		{
+			$user_ids[] = $user
+				->data->ID;
+			$user_names[] = $user
+				->data->user_login;
+		}
+
+		$dir_obj = $this->get_nab_path_and_file();
+		$file_path = $dir_obj['path'];
+		$file_name = $dir_obj['filename'];
+		?>
+
+		<div class="wrap">
+
+			<h2 align="center">Export Data from your Site</h2>
+
+			<div class="eauWrapper">
+				<div id="eauMainContainer" class="postbox eaucolumns">
+
+					<div class="inside">
+
+						<form id="infoForm" method="post">
+
+							<?php wp_nonce_field('export_path_from_urls'); ?>
+
+							<table class="form-table">
+
+								<tr style="display: none;">
+
+									<th>Select a Post Type to Extract Data: </th>
+
+									<td>
+
+										<label><input type="radio" name="post-type" value="any" required="required" checked /> All
+											Types (pages, posts, and custom post types)</label><br/>
+										<label><input type="radio" name="post-type" value="page" required="required"/> Pages</label><br/>
+										<label><input type="radio" name="post-type" value="post" required="required"/> Posts</label><br/>
+
+										<?php
+											if (!empty($custom_posts_names) && !empty($custom_posts_labels))
+											{
+												for ($i = 0;$i < count($custom_posts_names);$i++)
+												{
+													echo '<label><input type="radio" name="post-type" value="' . esc_attr($custom_posts_names[$i]) . '" required="required" /> ' . esc_html($custom_posts_labels[$i]) . ' Posts</label><br>';
+												}
+											}
+										?>
+
+									</td>
+
+								</tr>
+
+								<tr style="display: none;">
+
+									<th>Export Fields:</th>
+
+									<td>
+
+										<label><input type="checkbox" name="additional-data[]" checked value="postIDs"/>
+											Post IDs</label><br/>
+										<label><input type="checkbox" name="additional-data[]" checked value="post_type"/>
+											post Type</label><br/>
+										<label><input type="checkbox" name="additional-data[]" checked value="path"/>
+											Paths</label><br/>
+
+									</td>
+
+								</tr>
+
+								<tr style="display: none;">
+
+									<th>Post Status:</th>
+
+									<td>
+
+										<label><input type="radio" name="post-status" value="publish"/>
+											Published</label><br/>
+										<label><input type="radio" name="post-status" value="pending"/> Pending</label><br/>
+										<label><input type="radio" name="post-status" value="draft"/> Draft & Auto
+											Draft</label><br/>
+										<label><input type="radio" name="post-status" value="future"/> Future
+											Scheduled</label><br/>
+										<label><input type="radio" name="post-status" value="private"/> Private</label><br/>
+										<label><input type="radio" name="post-status" value="trash"/> Trashed</label><br/>
+										<label><input type="radio" name="post-status" checked value="all"/> All (Published, Pending,
+											Draft, Future Scheduled, Private & Trash)</label><br/>
+
+									</td>
+
+								</tr>
+
+								<tr style="display: none">
+									<th></th>
+									<td><a href="#" id="moreFilterOptionsLabel"
+										onclick="lessFilterOptions(); return false;">Hide Filter Options</a></td>
+								</tr>
+
+								<tr>
+
+									<th>Date Range:</th>
+
+									<td>
+								<?php
+								$posts_from = "2000-01-01";
+								$posts_upto = "2022-01-01";
+								if (isset($_REQUEST['_wpnonce'])){
+									if(!empty($_POST['posts-from'])) $posts_from = sanitize_file_name($_POST['posts-from']);
+									if(!empty($_POST['posts-upto'])) $posts_upto = sanitize_file_name($_POST['posts-upto']);
+								}
+								?>
+										<label>From:<input type="date" id="posts-from" name="posts-from" value="<?php echo esc_attr($posts_from); ?>"
+														onmouseleave="setMinValueForPostsUptoField()"
+														onfocusout="setMinValueForPostsUptoField()"/></label>
+										<label>To:<input type="date" id="posts-upto" name="posts-upto" value="<?php echo esc_attr($posts_upto); ?>"/></label><br/>
+
+
+									</td>
+
+								</tr>
+
+								<tr style="display: none">
+
+									<th>By Author:</th>
+
+									<td>
+
+										<label><input type="radio" name="post-author" checked value="all"
+													required="required"/> All</label><br/>
+										<?php
+												if (!empty($user_ids) && !empty($user_names))
+												{
+													for ($i = 0;$i < count($user_ids);$i++)
+													{
+														echo '<label><input type="radio" name="post-author" value="' . esc_attr($user_ids[$i]) . '" required="required" /> ' . esc_html($user_names[$i]) . '</label><br>';
+													}
+												}
+											?>
+
+									</td>
+
+								</tr>
+
+								<tr style="display: none">
+									<th></th>
+									<td><a href="#" id="advanceOptionsLabel" onclick="showAdvanceOptions(); return false;">Show Advanced Options</a></td>
+								</tr>
+
+								<tr class="advance-options" style="display: none">
+
+									<th>Remove WooCommerce Extra Attributes: </th>
+
+									<td>
+
+										<label><input type="checkbox" name="remove-woo-attributes" value="yes"/> Yes &nbsp;&nbsp;<code>WooCommerce stores product attributes along with product categories, by default plugin may extract those attributes and show as categories. That can be fixed by enabling this option.</code>
+
+									</td>
+
+								</tr>
+
+								<tr class="advance-options" style="display: none">
+
+									<th>Exclude Domain URL: </th>
+
+									<td>
+
+										<label><input type="checkbox" name="exclude-domain" value="yes"/> Yes &nbsp;&nbsp;<code>Enabling this option will use relative URLs, by removing domain url (e.g. example.com/sample-post/ will become /sample-post/)</code>
+
+									</td>
+
+								</tr>
+
+								<tr>
+
+									<th>Number of Posts: <a href="#"
+															title="Specify Post Range to Extract, It is very useful in case of Memory Out Error!"
+															onclick="return false">?</a></th>
+
+									<td>
+
+										<label><input type="radio" name="number-of-posts" checked value="all"
+													required="required" onclick="hideRangeFields()"/> All</label><br/>
+										<label><input type="radio" name="number-of-posts" value="range" required="required"
+													onclick="showRangeFields()"/> Specify Range</label><br/>
+
+										<div id="postRange" style="display: table-row">
+											From: <input type="number" name="starting-point" placeholder="0">
+											To: <input type="number" name="ending-point" placeholder="500">
+										</div>
+
+									</td>
+
+								</tr>
+
+								<tr  style="display: none">
+									<td>
+									<input
+														type="text" name="csv-file-name" placeholder="An Error Occured"
+														value="<?php echo esc_attr($file_name); ?>"
+														size="30%"/>
+									</td>
+								</tr>
+								<tr >
+
+									<th>CSV File Name: </th>
+
+									<td>
+
+
+
+										<label><input
+													type="text" value="<?php echo esc_attr($file_name); ?>"
+													disabled size="30%"/></label>
+													<br/>
+													<code><?php echo esc_html($file_path); ?></code>
+
+
+									</td>
+
+
+								</tr>
+
+								<tr style="display: none">
+
+									<th>Export Type:</th>
+
+									<td>
+
+										<label><input type="radio" name="export-type" value="text" required="required" checked/> CSV File</label><br/>
+										<label><input type="radio" name="export-type" value="here" required="required" />
+											Output here</label><br/>
+
+									</td>
+
+								</tr>
+
+								<tr>
+
+									<td></td>
+
+									<td>
+										<input type="submit" name="export" class="button button-primary"
+											value="Export CSV File Now"/>
+									</td>
+
+								</tr>
+
+								<tr>
+
+									<td></td>
+									<td><strong>NOTE: <em><ul><li>- If successfully created an Export Personal Data request, if redownloading of exported CSV file  is needed. Just navigate to Export Personal Data under Tools Admin Menu/Left Nav Bar</li><li>- If already created a request. Just navigate to Export Personal Data, same navigation as stated above and try to locate you request. It can be identified by email of those user's requests</li></ul></em></strong></td>
+								</tr>
+
+							</table>
+						</form>
+
+
+					</div>
+
+				</div>
+				<div id="eauSideContainer" class="eaucolumns">
+					<div class="postbox">
+						<h3>Want to Support?</h3>
+						<div class="inside">
+							<p>If you enjoyed the plugin, and want to support:</p>
+							<ul>
+								<li>
+									<a href="https://AtlasGondal.com/contact-me/?utm_source=self&utm_medium=wp&utm_campaign=export-all-urls&utm_term=hire-me"
+									target="_blank">Hire me</a> on a project
+								</li>
+								<li>Buy me a Coffee
+									<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=YWT3BFURG6SGS&source=url" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif"/> </a>
+
+								</li>
+							</ul>
+							<hr>
+							<h3>Wanna say Thanks?</h3>
+							<ul>
+								<li>Leave <a
+											href="https://wordpress.org/support/plugin/export-all-urls/reviews/?filter=5#new-post"
+											target="_blank">&#9733;&#9733;&#9733;&#9733;&#9733;</a> rating
+								</li>
+								<li>Tweet me: <a href="https://twitter.com/atlas_gondal" target="_blank">@Atlas_Gondal</a>
+								</li>
+							</ul>
+							<hr>
+							<h3>Got a Problem?</h3>
+							<p>If you want to report a bug or suggest new feature. You can:</p>
+							<ul>
+								<li>Create <a href="https://wordpress.org/support/plugin/export-all-urls/" target="_blank">Support
+										Ticket</a></li>
+
+								<li>Write me an <a
+											href="https://AtlasGondal.com/contact-me/?utm_source=self&utm_medium=wp&utm_campaign=export-all-urls&utm_term=write-an-email"
+											target="_blank">Email</a></li>
+							</ul>
+							<strong>Reporting</strong> an issue is way better than leaving <strong>1 star</strong> feedback, which does not help you, me, or the community. So, please consider giving me a chance to help before leaving any negative feedback.
+							<hr>
+							<h4 id="eauDevelopedBy">Developed by: <a
+										href="https://AtlasGondal.com/?utm_source=self&utm_medium=wp&utm_campaign=export-all-urls&utm_term=developed-by"
+										target="_blank">Atlas Gondal</a></h4>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<style>.eauWrapper{display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap;overflow:hidden}#eauMainContainer{width:75%;margin-bottom:0}#eauSideContainer{width:24%}#eauSideContainer .postbox:first-child{margin-left:20px;padding-top:15px}.eaucolumns{float:left;display:-webkit-flex;display:-ms-flexbox;display:flex;margin-top:5px}#eauSideContainer .postbox{margin-bottom:0;float:none}#eauSideContainer .inside{margin-bottom:0}#eauSideContainer hr{width:70%;margin:30px auto}#eauSideContainer h3{cursor:default;text-align:center;font-size:16px}#eauSideContainer li{list-style:disclosure-closed;margin-left:25px}#eauSideContainer li a img{display:inline-block;vertical-align:middle}#eauDevelopedBy{text-align:center}#outputData{border-collapse:collapse;width:98%}#outputData tr:nth-child(even){background-color:#fff}#outputData tr:hover{background-color:#ddd}#outputData th{background-color:#000;color:#fff}#outputData td,#outputData th{text-align:left;padding:8px}#outputData th:first-child{width:4%}#outputData #postID{width:10%}#outputData #postType{width:10%}#outputData #postPath{width:30%}#outputData #eauMainContainer code {font-size: 11px;background-color: #eee;padding-left: 5px;padding-right: 5px;}</style>
+
+			<script type="text/javascript">
+				function showRangeFields() {
+					document.getElementById('postRange').style.display = 'block';
+				}
+
+				function hideRangeFields() {
+					document.getElementById('postRange').style.display = 'none';
+				}
+
+				function showAdvanceOptions() {
+
+					var rows = document.getElementsByClassName('advance-options');
+
+					for (var i = 0; i < rows.length; i++) {
+						rows[i].style.display = 'table-row';
+					}
+
+					document.getElementById('advanceOptionsLabel').innerHTML = "Hide Advanced Options";
+					document.getElementById('advanceOptionsLabel').setAttribute("onclick", "javascript: hideAdvanceOptions(); return false;");
+
+				}
+
+				function hideAdvanceOptions() {
+
+					var rows = document.getElementsByClassName('advance-options');
+
+					for (var i = 0; i < rows.length; i++) {
+						rows[i].style.display = 'none';
+					}
+
+					document.getElementById('advanceOptionsLabel').innerHTML = "Show Advanced Options";
+					document.getElementById('advanceOptionsLabel').setAttribute("onclick", "javascript: showAdvanceOptions(); return false;");
+
+				}
+
+				function moreFilterOptions() {
+					var rows = document.getElementsByClassName('filter-options');
+
+					for (var i = 0; i < rows.length; i++) {
+						rows[i].style.display = 'table-row';
+					}
+
+					document.getElementById('moreFilterOptionsLabel').innerHTML = "Hide Filter Options";
+					document.getElementById('moreFilterOptionsLabel').setAttribute("onclick", "javascript: lessFilterOptions(); return false;");
+
+				}
+
+				function lessFilterOptions() {
+					var rows = document.getElementsByClassName('filter-options');
+
+					for (var i = 0; i < rows.length; i++) {
+						rows[i].style.display = 'none';
+					}
+
+					document.getElementById('moreFilterOptionsLabel').innerHTML = "Show Filter Options";
+					document.getElementById('moreFilterOptionsLabel').setAttribute("onclick", "javascript: moreFilterOptions(); return false;");
+
+				}
+
+				function setMinValueForPostsUptoField() {
+					console.log(document.getElementById('posts-from').value);
+					if (document.getElementById('posts-from').value != "") {
+						document.getElementById('posts-upto').setAttribute('min', document.getElementById('posts-from').value);
+					}
+
+				}
+			</script>
+
+
+		</div>
+
+
+		<?php
+		if (isset($_POST['export']))
+		{
+
+			if (isset($_REQUEST['_wpnonce']))
+			{
+				if (!wp_verify_nonce(sanitize_text_field($_REQUEST['_wpnonce']), 'export_path_from_urls'))
+				{
+					echo "<div class='notice notice-error' style='width: 93%'>Security token validation failed!</div>";
+					exit;
+				}
+
+				if (!empty($_POST['post-type']) && !empty($_POST['export-type']) && !empty($_POST['additional-data']) && !empty($_POST['post-status']) && !empty($_POST['post-author']) && !empty($_POST['number-of-posts']))
+				{
+
+					$post_type = sanitize_text_field($_POST['post-type']);
+					$export_type = sanitize_text_field($_POST['export-type']);
+					$additional_data = map_deep($_POST['additional-data'], 'sanitize_text_field');
+					$post_status = sanitize_text_field($_POST['post-status']);
+					$post_author = sanitize_text_field($_POST['post-author']);
+					$remove_woo_attributes = isset($_POST['remove-woo-attributes']) ? sanitize_text_field($_POST['remove-woo-attributes']) : null;
+					$exclude_domain = isset($_POST['exclude-domain']) ? sanitize_text_field($_POST['exclude-domain']) : null;
+					$number_of_posts = sanitize_text_field($_POST['number-of-posts']);
+					$csv_name = !empty($_POST['csv-file-name']) ? sanitize_file_name($_POST['csv-file-name']) : "";
+
+					if ($number_of_posts == "range")
+					{
+						$offset = !empty($_POST['starting-point']) ? absint(sanitize_text_field($_POST['starting-point'])) : "all";
+						$post_per_page = !empty($_POST['ending-point']) ? absint(sanitize_text_field($_POST['ending-point'])) : "all";
+
+						if (!isset($offset) || !isset($post_per_page))
+						{
+							echo "Sorry, you didn't specify starting and ending post range. Please <strong>Set Post Range</strong> OR <strong>Select All</strong> and try again! :)";
+							exit;
+						}
+
+						$post_per_page = $post_per_page - $offset;
+
+					}
+					else
+					{
+						$offset = 'all';
+						$post_per_page = 'all';
+					}
+
+					if ($export_type == 'text')
+					{
+						if (empty($csv_name))
+						{
+							echo "Invalid/Missing CSV File Name!";
+							exit;
+						}
+					}
+
+
+
+					if (!empty($posts_from) && !empty($posts_upto))
+					{
+
+						if ($posts_from > $posts_upto)
+						{
+							echo "Sorry, invalid post date range. :)";
+							exit;
+						}
+
+					}
+					else
+					{
+						$posts_from = '';
+						$posts_upto = '';
+					}
+
+					$selected_post_type = $this->eau_get_selected_post_type($post_type, $custom_posts_names);
+
+					$this->eau_generate_output($selected_post_type, $post_status, $post_author, $remove_woo_attributes, $exclude_domain, $post_per_page, $offset, $export_type, $additional_data, $csv_name, $posts_from, $posts_upto);
+
+				}
+				else
+				{
+					echo "<div class='notice notice-error' style='width: 93%'>Sorry, you missed something, Please recheck above options, especially <strong>Export Fields</strong> and try again! :)</div>";
+					exit;
+				}
+			}
+			else
+			{
+				echo "<div class='notice notice-error' style='width: 93%'>Verification token is missing!</div>";
+				exit;
+			}
+
+		}
+		else if ((isset($_REQUEST['del']) && $_REQUEST['del'] == 'y'))
+		{
+			if (!isset($_REQUEST['_wpnonce']) || (!empty(sanitize_text_field($_GET['_wpnonce'])) && !wp_verify_nonce(sanitize_text_field($_GET['_wpnonce']))))
+			{
+				echo "You are not authorized to perform this action!";
+				exit();
+			}
+			else if(isset($_REQUEST['del']) && $_REQUEST['del'] == 'y')
+			{
+				$exportmeta = $this->get_nab_path_and_file();
+				$file_dir = $exportmeta['path'];
+				$checkfile = !empty($_REQUEST['f']) ? base64_decode(sanitize_text_field($_REQUEST['f'])) : null;
+				$file = "$file_dir/$checkfile";
+				echo (!empty($file) ? (file_exists($file) ? (!unlink($file) ? "<div class='notice notice-error' style='width: 97%'></div>Unable to delete file, please delete it manually!" : "<div class='updated' style='width: 97%'>You did great, the file was <strong>Deleted Successfully</strong>!</div>") : null) : "<div class='notice notice-error'>Missing file path.</div>"); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.file_ops_unlink
+			}
+		}
+	}
+}
+
+new ExportAllPathFromUrlsSettings();
---
+++ nab-export-path-from-urls/nab-export-path-from-urls.php
@@ -0,0 +1,110 @@
+<?php
+/*
+Plugin Name: Export Paths from URLs
+Description: Export a CSV file that contains the following columns: - Path (the path, not the URL - e.g. /company/cognizant-technology-solutions-u-s-corporation/ not https://amplify.nabshow.com/company/cognizant-technology-solutions-u-s-corporation/, - Post ID - the Post ID of the content on that page, - Post Type - e.g. Page, Company, Session, Article, etc. The CSV  content will show up before or on top of footer section in page once exported CSV file  exists. This plugin  creates a user request upon  export/navigating to export path amdin tool page. It utilize WP core Export Personal Data feature so can always navigate to Export Personal Data page under tool menu in  dmin Menu/Nav bar and can download the exported  csv file if user request is created.
+Version: 1.0.1
+Author: Crush & Lovely
+Contributor:Nab
+*/
+namespace Plugins\NabExportPathFromUrls;
+
+require_once (WP_PLUGIN_DIR . '/nab-export-path-from-urls/cls-export-all-paths.php');
+
+// add_action( 'admin_init', [new NabExportPathFromUrls, 'init_privacy_compat']);
+class NabExportPathFromUrls extends \Plugins\NabExportPathFromUrls\ExportAllPaths
+{
+	public function __construct()
+	{
+		// register_activation_hook( __FILE__, array( 'ExportAllPaths', 'plugin_activation' ) );
+		// register_deactivation_hook( __FILE__, array( 'ExportAllPaths', 'plugin_deactivation' ) );
+
+		if (is_admin()) {
+
+			// Display a link to the VIP/Automattic Privacy Policy if the site doesn't already define one.
+			add_action( 'the_privacy_policy_link', __NAMESPACE__ . '\the_vip_privacy_policy_link', PHP_INT_MAX ); // Hook in later so we don't override existing filters
+
+			add_filter('admin_menu', [ $this, 'export_paths_from_urls_nav']);
+
+			add_action( 'admin_init', [ $this, 'export_paths_from_urls_activation'] );
+
+			add_action( 'admin_init', [$this, 'init_privacy_compat']);
+
+		}else{
+			add_filter('the_content',[$this, 'nabshow_content_after_body']);
+		}
+
+		add_action( 'init', [$this, 'init_privacy_compat_cleanup']);
+
+	}
+
+	public function init_privacy_compat() {
+		// Replace core's privacy data export handler with a custom one.
+		// remove_action( 'wp_privacy_personal_data_export_file', 'wp_privacy_generate_personal_data_export_file', 10 );
+		remove_action( 'wp_privacy_personal_data_export_file', 'wp_privacy_generate_personal_data_export_file', 10 );
+		add_action( 'wp_privacy_personal_data_export_file', [$this, 'nab_generate_personal_data_export_file'] );
+
+	}
+
+	public function init_privacy_compat_cleanup() {
+		// Replace core's privacy data delete handler with a custom one.
+		remove_action( 'wp_privacy_delete_old_export_files', 'wp_privacy_delete_old_export_files' );
+		add_action( 'wp_privacy_delete_old_export_files', [$this, 'delete_old_export_files']  );
+	}
+
+	public function export_paths_from_urls_nav(){
+
+		add_management_page( 'Export Paths from URLs', 'Export Paths from URLs', 'manage_options', 'extract-paths-from-urls-settings', [$this, 'export_path_from_urls_settings_page'], null);
+
+	}
+	public function export_path_from_urls_settings_page(){
+
+		require WP_PLUGIN_DIR . '/nab-export-path-from-urls/cls-export-all-path-from-urls-settings.php';
+	}
+
+	public function export_paths_from_urls_activation() {
+		if ( ! get_transient( 'eau_export_all_urls_activation_redirect' ) ) {
+			return;
+		}
+
+		delete_transient( 'eau_export_all_urls_activation_redirect' );
+
+		wp_safe_redirect( add_query_arg( array( 'page' => 'extract-all-urls-settings' ), admin_url( 'tools.php' )) );
+		exit;
+
+	}
+
+	// =================
+
+	// This function attached on the_content hook is intended to display the CSV Content.
+	// Can be In Admin or anywhere from site.. That includes Nabshow Amplify
+	public function nabshow_content_after_body($content){
+
+		$rowdata = $this->retrieve_exported_file_to_array();
+
+		if(empty($rowdata)) return $content;
+
+		$rowspagination = $this->get_rows_pagination($rowdata);
+
+		$content = $rowspagination;
+
+		$escjs =  "<script>
+					jQuery(function(){
+						var body = document.body,
+						html = document.documentElement;
+
+						var height = Math.max( body.scrollHeight, body.offsetHeight,
+										html.clientHeight, html.scrollHeight, html.offsetHeight );
+						var scroll_pos=(height);
+						jQuery('html, body').animate({scrollTop:(scroll_pos)}, '2000');
+					});
+				</script>";
+
+		$content .= $escjs;
+
+		return $content;
+	}
+
+
+}
+
+new NabExportPathFromUrls();
