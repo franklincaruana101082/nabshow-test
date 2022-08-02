55d10b1ac78d76284574f3a21ab84b3ddf39a691
franklin caruana <franklin@solveware.co>
2022-08-03 00:47:23 +0800

    fix: mkdir removed due to permission

--- plugins/nab-export-path-from-urls/classes/cls-export-meta.php
+++ plugins/nab-export-path-from-urls/classes/cls-export-meta.php
@@ -57,7 +57,7 @@ class ExportMeta
 			$local_export_pathname = "/wp/".substr( $exports_dir, 6 );
 
 			// Create the folder path.
-			$local_export_dir_created = wp_mkdir_p( $local_export_pathname );
+			if(!file_exists($local_export_pathname)) $local_export_dir_created = wp_mkdir_p( $local_export_pathname );
 			if ( is_wp_error( $local_export_dir_created ) ) {
 				wp_send_json_error( $local_export_dir_created->get_error_message() );
 			}
